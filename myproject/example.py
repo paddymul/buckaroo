f170a8634b13bc34d3b291224914e130912f8a0b
Paddy Mullen <paddy@paddymullen.com>
2023-03-31 14:36:45 -0400

    sending empty commands results in the original untouched dataframe being returned

--- myproject/example.py
+++ myproject/example.py
@@ -31,6 +31,7 @@ class ExampleWidget(DOMWidget):
 
     js_df = Dict({}).tag(sync=True)
     transformed_df = Dict({}).tag(sync=True)
+    transform_error = Unicode('').tag(sync=True)
     
     def __init__(self, df):
         super().__init__()
@@ -39,9 +40,18 @@ class ExampleWidget(DOMWidget):
 
     @observe('commands')
     def interpret_commands(self, change):
-        commands = change['new']
-        transformed_df = dcf_transform(commands, self.df)
-        self.transformed_df = json.loads(transformed_df.to_json(orient='table', indent=2))
+        try:
+            commands = change['new']
+            if len(commands) == 1:
+                self.transform_error = "matched"
+                self.transformed_df = self.js_df
+                return
+            transformed_df = dcf_transform(commands, self.df)
+            self.transformed_df = json.loads(transformed_df.to_json(orient='table', indent=2))
+            self.transform_error = ''
+        except Exception as e:
+            self.transform_error = str(e)
+        
 
         
         
