5bb88975bac672e37276670e88be2b88d5bf5e45
Paddy Mullen <paddy@paddymullen.com>
2023-04-10 11:53:03 -0400

    first release to testpypi of dcf, is installable and works

--- full_build.sh
+++ full_build.sh
@@ -5,4 +5,4 @@ rm -rf dist
 python -m build .
 rm dist/index.js dist/index.js.LICENSE.txt
 twine check dist/*
-#python -m twine upload --repository testpypi dist/*
+python -m twine upload --repository testpypi dist/*
