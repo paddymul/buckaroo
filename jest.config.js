62b69df4134fd4ba9bbf693d9d85e1028e77a262
Paddy Mullen <paddy@paddymullen.com>
2023-05-04 10:37:19 -0400

    jest tests pass locally, empty, but still in place for CI

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,6 @@ module.exports = {
   },
   transformIgnorePatterns: ['node_modules/?!(@jupyter-widgets)'],
   testPathIgnorePatterns: ['ui-tests-ipw7/', 'ui-tests-ipw8/'],
-  setupFiles: ['./tests/js/setupFile.js'],
-  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
   },
