9e095bc0ad1b1dc613edc2e952c4a0248cb6c3f5
Paddy Mullen <paddy@paddymullen.com>
2023-07-16 11:50:08 -0400

    build 0.3.8 release

--- full_build.sh
+++ full_build.sh
@@ -5,5 +5,6 @@ set -e
 python -m build .
 rm -f dist/index.js dist/index.js.LICENSE.txt
 twine check dist/*
-python -m twine upload --repository testpypi dist/*
+#python -m twine upload --repository testpypi dist/*
 python -m twine upload --repository pypi dist/*
+npm publish
