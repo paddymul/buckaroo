f6150b35d60fd1f089ac178dfeb06319cf609503
Paddy Mullen <paddy@paddymullen.com>
2023-07-19 18:05:30 -0400

    WIP

--- buckaroo/analysis.py
+++ buckaroo/analysis.py
@@ -93,6 +93,41 @@ class DefaultSummaryStats(ColAnalysis):
             nan_per=nan_count/l,
             mode=get_mode(ser))
 
+
+class DefaultSummaryStats(ColAnalysis):
+    provided_summary = [
+        'length', 'min', 'max', 'mean', 'nan_count', 'distinct_count',
+        'distinct_per', 'empty_count', 'empty_per', 'unique_per', 'nan_per',
+        'mode']
+    
+    @staticmethod
+    def summary(sampled_ser, summary_ser, ser):
+
+
+def int_digits(n):
+    if n == 0:
+        return 1
+    if np.sign(n) == -1:
+        return int(np.floor(np.log10(np.abs(n)))) + 2
+    return int(np.floor(np.log10(n)+1))
+
+
+def histogram(ser):
+    raw_counts, bins = np.histogram(ser, 10)
+    scaled_counts = np.round(raw_counts/raw_counts.sum(),2)
+    return [scaled_counts, bins]
+
+def table_sumarize_num_ser(ser):
+    if len(ser) == 0:
+        return dict(is_numeric=False)
+    return dict(
+        is_numeric=True,
+        is_integer=pd.api.types.is_integer_dtype(ser),
+        min_digits=int_digits(ser.min()),
+        max_digits=int_digits(ser.max()),
+        histogram=histogram(ser))
+
+
 def summarize_numeric(ser):
 
     num_stats = summarize_string(ser)
@@ -110,4 +145,33 @@ def summarize_column(ser):
         return summarize_string(ser)
 
 
+
+class DfStats(object):
+    def __init__(self,
+            df,
+            annotate_funcs=[add_col_rankings],
+            # summary_func=summary_stats.summarize_df,
+            # order_col_func=summary_stats.order_columns):
+            summary_func=summarize_df,
+            order_col_func=order_columns):
+
+        rows = len(df)
+        cols = len(df.columns)
+        item_count = rows * cols
+
+
+        if item_count > FAST_SUMMARY_WHEN_GREATER:
+            df = df.sample(np.min([50_000, len(df)]))
+        self.df = df
+        self.sdf = summary_func(df)
+        for func in annotate_funcs:
+            func(df, self.sdf)
+        try:
+            self.cpd = get_cor_pair_dict(self.df, self.sdf)
+            self.col_order = order_col_func(self.sdf, self.cpd)
+        except Exception as e:
+            print(e)
+            self.col_order = self.df.columns
+
+        
 
