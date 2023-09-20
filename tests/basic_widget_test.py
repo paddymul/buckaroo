e4b0cd6c2ed55de3cfc41cbfd0fbee6eafcfb1f2
Paddy Mullen <paddy@paddymullen.com>
2023-09-20 09:11:50 -0400

    test of new meta key for symbols

--- tests/basic_widget_test.py
+++ tests/basic_widget_test.py
@@ -24,6 +24,24 @@ def test_interpreter():
     field_names = [ f['name'] for f in tdf['schema']['fields'] ]
     assert 'starttime' not in field_names
 
+def test_symbol_meta():    
+    """verifies that a symbol with a meta key can be added and
+    properly interpretted.  This should probably be a lower level
+    parser test
+
+    """
+
+
+    df = pd.read_csv('./examples/data/2014-01-citibike-tripdata.csv')
+    w = BuckarooWidget(df)
+    assert w.operation_results['generated_py_code']  == '#from py widget init'
+    w.operations = [[{"symbol":"dropcol", "meta":{}},{"symbol":"df"},"starttime"]]
+
+    tdf = w.operation_results['transformed_df']
+    assert w.operation_results['transform_error'] == False
+    field_names = [ f['name'] for f in tdf['schema']['fields'] ]
+    assert 'starttime' not in field_names
+
 
 def test_interpreter_errors():
     df = pd.read_csv('./examples/data/2014-01-citibike-tripdata.csv')
