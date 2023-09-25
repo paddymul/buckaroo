257628f9cd6b0636d1f467a0d6f6e4bfb92849b8
Paddy Mullen <paddy@paddymullen.com>
2023-09-25 09:45:38 -0400

    more tests pass

--- tests/auto_clean_test.py
+++ tests/auto_clean_test.py
@@ -29,13 +29,13 @@ MIXED_NUMERIC_META = {'bool': 0.0, 'bool_error': 1.0,
                       'float': (2/3), 'float_error': (1/3),
                       'int': (2/3), 'int_error': (1/3)}
 
-
 def assign_values(d, new_values):
     d2 = d.copy()
     d2.update(new_values)
     return d2
 
-DATETIME_DTYPE_META = assign_values(ac.default_type_dict, {'general_type':'datetime', 'exact_type': 'datetime64[ns]'})
+DATETIME_DTYPE_META = assign_values(ac.default_type_dict,
+                                    {'general_type':'datetime', 'exact_type': 'datetime64[ns]'})
 
 MIXED_EXACT = assign_values(ac.default_type_dict, {'exact_type': 'Int64', 'general_type': 'int', 'int':1})
 
@@ -45,10 +45,8 @@ MIXED_EXACT = assign_values(ac.default_type_dict, {'exact_type': 'Int64', 'gener
 #                       'int': (2/3), 'int_error': (1/3)}
 
 
-
-
 def test_get_typing_metadata():
-    #assert WEIRD_INT == ac.get_typing_metadata(pd.Series([5, 2, 3.1, None, NA]))
+    # assert WEIRD_INT == ac.get_typing_metadata(pd.Series([5, 2, 3.1, None, NA]))
     assert INT_META == ac.get_typing_metadata(pd.Series(['181', '182', '183', 'a']))
     assert FLOAT_META == ac.get_typing_metadata(pd.Series(['181.1', '182.2', '183', 'a']))
     assert STRING_META == ac.get_typing_metadata(pd.Series(['181.1', 'b', 'c', 'a']))
@@ -59,7 +57,6 @@ def test_get_typing_metadata():
     assert MIXED_EXACT == ac.get_typing_metadata(pd.Series([NA, 2, 3, NA, NA], dtype='Int64'))
     assert MIXED_NUMERIC_META == ac.get_typing_metadata(pd.Series(['a', 2.0, 3.0, None, NA]))
     
-
     #there are still problems here, the code isn't properly distinguishing bools from ints and bools
     assert BOOL_META == ac.get_typing_metadata(pd.Series(['a', 'b', False, True, False]))
 
@@ -114,12 +111,16 @@ def atest_autotype_df():
                 'int':pd.Series(['a', 2, 3, 4, None]),
                 'bool':pd.Series(['a', False, True, None]),
                 'int2':pd.Series(['a', 2.0, 3.0, None, NA]),
-                'float':pd.Series(['a', 2.0, 3.1, None, NA])
+                'float':pd.Series(['a', 2.0, 3.1, None, NA]),
+                'allNA':pd.Series([NA, NA, NA,  None, NA]),
+                'string_':pd.Series(['a', 'b', 'c', 'd', 'e'])
                 }
         )),
             pd.DataFrame({
                 'int' :  pd.Series([NA, 2,3,4, NA], dtype='UInt8'),
                 'bool':  pd.Series([NA, False, True, NA], dtype='boolean'),
                 'int2':  pd.Series([NA, 2, 3, NA, NA], dtype='UInt8'),
-                'float': pd.Series([nan, 2, 3.1, nan, nan], dtype='float')
+                'float': pd.Series([nan, 2, 3.1, nan, nan], dtype='float'),
+                'allNA':pd.Series([NA, NA, NA,  None, NA]),
+                'string_':pd.Series(['a', 'b', 'c', 'd', 'e'])
             }))
