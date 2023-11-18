684caefe2c6734c386422f827b21fb8a653f22b4
Paddy Mullen <paddy@paddymullen.com>
2023-11-18 15:48:57 -0500

    added test for to_string

--- tests/unit/commands/cleaning_command_test.py
+++ tests/unit/commands/cleaning_command_test.py
@@ -45,34 +45,12 @@ def test_to_float():
         output_df['mixed_floats'],
         pd.Series([3, 4, 7.1, np.nan, np.nan], dtype='float64', name='mixed_floats'))
 
+def test_to_string():
+    base_df = pd.DataFrame({
+        'mixed_strings':['a', 'b', pd.NA], 'b': [pd.NA] * 3})
     
+    output_df = same(to_string, [[s('to_string'), s('df'), "mixed_strings"]], base_df)
+    pd.testing.assert_series_equal(
+        output_df['mixed_strings'],
+        pd.Series(['a', 'b', pd.NA], dtype='string', name='mixed_strings'))
 
-# def test_dropcol():
-#     base_df = pd.DataFrame({
-#         'a':np.random.randint(1, 10, 5), 'b':np.random.randint(1, 10, 5),
-#         'c':np.random.randint(1, 10, 5)})
-
-#     same(DropCol, [[s('dropcol'), s('df'), "a"]], base_df)
-
-# def test_onehot():
-#     base_df = pd.DataFrame({
-#         'a':['cc', 'cc', 'dd', 'ee', 'ff'], 'b': [pd.NA, 2, 2, 2, pd.NA]})
-    
-#     output_df = same(OneHot, [[s('onehot'), s('df'), "a"]], base_df)
-#     assert output_df.columns.to_list() == ['b', 'cc', 'dd', 'ee', 'ff']
-    
-# def test_groupby():
-#     base_df = pd.DataFrame({
-#         'a':['cc', 'cc', 'cc', 'ee', 'ff'], 'b': [pd.NA, 2, 2, 2, pd.NA]})
-    
-#     output_df = same(GroupBy, [[s('groupby'), s('df'), "a", {'b':'count'}]], base_df)
-#     expected_output = pd.DataFrame({'b': {'cc': 2, 'ee': 1, 'ff': 0}},
-#                                    index=pd.Index(['cc', 'ee', 'ff'], dtype='object', name='a'))
-#     pd.testing.assert_frame_equal(output_df, expected_output)
-    
-# def test_reindex():
-#     base_df = pd.DataFrame({
-#         'a':['ca', 'cb', 'cd', 'ee', 'ff'], 'b': [pd.NA, 2, 2, 2, pd.NA]})
-    
-#     output_df = same(reindex, [[s('reindex'), s('df'), "a"]], base_df)
-#     assert output_df.index.to_list() == ['ca', 'cb', 'cd', 'ee', 'ff']
