3e6294a559ad8e13450a42feb192aea971dd30fc
Paddy Mullen <paddy@paddymullen.com>
2023-11-18 18:50:57 -0500

    ruff fixes

--- buckaroo/customizations/polars_commands.py
+++ buckaroo/customizations/polars_commands.py
@@ -1,9 +1,9 @@
 import polars as pl
-import numpy as np
+#import numpy as np
 
 from ..jlisp.lispy import s
-from ..jlisp.configure_utils import configure_buckaroo
-from ..auto_clean.cleaning_commands import (to_bool, to_datetime, to_int, to_float, to_string)
+#from ..jlisp.configure_utils import configure_buckaroo
+#from ..auto_clean.cleaning_commands import (to_bool, to_datetime, to_int, to_float, to_string)
 
 class Command(object):
     pass
@@ -35,49 +35,9 @@ class DropCol(Command):
     @staticmethod 
     def transform_to_py(df, col):
         return "    df.drop_in_place('%s')" % col
+
+
 '''
-class OneHot(Command):
-    command_default = [s('onehot'), s('df'), "col"]
-    command_pattern = [None]
-    @staticmethod 
-    def transform(df, col):
-        one_hot = pd.get_dummies(df[col])
-        df.drop(col, axis=1, inplace=True)
-        #how to make this inplace?
-        return df.join(one_hot) 
-
-    @staticmethod 
-    def transform_to_py(df, col):
-        commands = [
-            "    one_hot = pd.get_dummies(df['%s'])" % (col),
-            "    df.drop('%s', axis=1, inplace=True)" % (col),
-            "    df = df.join(one_hot)"]
-        return "\n".join(commands)
-
-group_df = pd.DataFrame({'a':[10,20,30,40,50,60],
-                   'b':[1,2,3,4,5,6],
-                   'c':['q', 'q', 'q', 'q', 'w', 'w']})
-
-def safe_int(x):
-    try:
-        return int(x)
-    except:
-        return np.nan
-
-class SafeInt(Command):
-    command_default = [s('safeint'), s('df'), "col"]
-    command_pattern = [None]
-
-    @staticmethod 
-    def transform(df, col):
-        df[col] = df[col].apply(safe_int)
-        #df[col] = 5
-        return df
-
-    @staticmethod 
-    def transform_to_py(df, col):
-        return "    df['%s'] = df['%s'].apply(safe_int)" % (col, col)
-
 
 class GroupBy(Command):
     command_default = [s("groupby"), s('df'), 'col', {}]
@@ -122,6 +82,48 @@ class GroupBy(Command):
         commands.append("    df = pd.DataFrame(df_contents)")
         return "\n".join(commands)
     
+class OneHot(Command):
+    command_default = [s('onehot'), s('df'), "col"]
+    command_pattern = [None]
+    @staticmethod 
+    def transform(df, col):
+        one_hot = pd.get_dummies(df[col])
+        df.drop(col, axis=1, inplace=True)
+        #how to make this inplace?
+        return df.join(one_hot) 
+
+    @staticmethod 
+    def transform_to_py(df, col):
+        commands = [
+            "    one_hot = pd.get_dummies(df['%s'])" % (col),
+            "    df.drop('%s', axis=1, inplace=True)" % (col),
+            "    df = df.join(one_hot)"]
+        return "\n".join(commands)
+
+group_df = pd.DataFrame({'a':[10,20,30,40,50,60],
+                   'b':[1,2,3,4,5,6],
+                   'c':['q', 'q', 'q', 'q', 'w', 'w']})
+
+def safe_int(x):
+    try:
+        return int(x)
+    except:
+        return np.nan
+
+class SafeInt(Command):
+    command_default = [s('safeint'), s('df'), "col"]
+    command_pattern = [None]
+
+    @staticmethod 
+    def transform(df, col):
+        df[col] = df[col].apply(safe_int)
+        #df[col] = 5
+        return df
+
+    @staticmethod 
+    def transform_to_py(df, col):
+        return "    df['%s'] = df['%s'].apply(safe_int)" % (col, col)
+
 
 
 class ato_datetime(Command):
