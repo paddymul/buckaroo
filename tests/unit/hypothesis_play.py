8f0e73eb8866e8f2c2cab507aed13f1ea4d2eaf7
Paddy Mullen <paddy@paddymullen.com>
2023-11-25 12:51:33 -0500

    more hypothesis tests

--- tests/unit/hypothesis_play.py
+++ tests/unit/hypothesis_play.py
@@ -5,62 +5,38 @@ import buckaroo
 from hypothesis import strategies as st
 
 
-'''
-import pandera as pa
-schema = pa.DataFrameSchema(
-    {
-        "val1": pa.Column(int, pa.Check.in_range(-2, 3)),
-        "val2": pa.Column(int, pa.Check.in_range(-2, 3)),
-        "str": pa.Column(str, pa.Check.in_range(-2, 3)),
-    }
-)
+def get_strategy(sz):
+    OPTIONAL_INTS = st.lists(st.one_of(
+        st.integers(min_value=-9223372036854775806, max_value=9223372036854775806),
+        st.none()), max_size=sz, min_size=sz)
 
-out_schema = schema.add_columns(
-    {
-        "val3": pa.Column(float, pa.Check.in_range(-2, 3)),
-    },
-)
+    OPTIONAL_FLOATS = st.lists(st.one_of(st.floats(), st.none()), max_size=sz, min_size=sz)
 
+    OPTIONAL_TEXT = st.lists(st.one_of(st.none(), st.text()), max_size=sz, min_size=sz)
 
+    OPTIONAL_DICTS = st.lists(
+        st.one_of(st.none(), st.dictionaries(st.text(), st.integers())),
+        max_size=sz, min_size=sz)
 
+    OPTIONAL_LISTS = st.lists(
+        st.one_of(st.none(), st.lists(st.text(), max_size=10, min_size=3)),
+        max_size=sz,
+        min_size=sz)
 
-@hypothesis.given(schema.strategy(size=5))
-def test_processing_fn(dataframe):
-    buckaroo.BuckarooWidget(dataframe, debug=True)
-    #processing_fn(dataframe)
-'''
+    OPTIONAL_ONE_OF_ALL = st.one_of(
+        OPTIONAL_DICTS, OPTIONAL_FLOATS,
+        OPTIONAL_INTS,
+        OPTIONAL_LISTS, OPTIONAL_TEXT)
+    return OPTIONAL_ONE_OF_ALL
 
-#copied from pandas 2.???
-#fails occasionally on my mac osx arm
-OPTIONAL_INTS = st.lists(st.one_of(st.integers(), st.none()), max_size=10, min_size=3)
-
-OPTIONAL_FLOATS = st.lists(st.one_of(st.floats(), st.none()), max_size=10, min_size=3)
-
-OPTIONAL_TEXT = st.lists(st.one_of(st.none(), st.text()), max_size=10, min_size=3)
-
-OPTIONAL_DICTS = st.lists(
-    st.one_of(st.none(), st.dictionaries(st.text(), st.integers())),
-    max_size=10,
-    min_size=3,
-)
-
-OPTIONAL_LISTS = st.lists(
-    st.one_of(st.none(), st.lists(st.text(), max_size=10, min_size=3)),
-    max_size=10,
-    min_size=3,
-)
-
-OPTIONAL_ONE_OF_ALL = st.one_of(
-    OPTIONAL_DICTS, #OPTIONAL_FLOATS,
-    OPTIONAL_INTS,
-    OPTIONAL_LISTS,# OPTIONAL_TEXT
-)
-
-@hypothesis.given(data=OPTIONAL_ONE_OF_ALL)
-def test_where_inplace_casting(data):
-    # GH 22051
-    df = pd.DataFrame({"a": data})
+@hypothesis.settings(deadline=10_000)
+@hypothesis.given(ser_1=get_strategy(10), ser_2=get_strategy(10))
+def test_buckaroo_hypothesis(ser_1, ser_2):
+    df = pd.DataFrame({"a": ser_1, "b":ser_2})
     buckaroo.BuckarooWidget(df, debug=True)
-    # df_copy = df.where(pd.notnull(df), None).copy()
-    # df.where(pd.notnull(df), None, inplace=True)
-    # tm.assert_equal(df, df_copy)
+
+@hypothesis.settings(deadline=10_000)
+@hypothesis.given(ser_1=get_strategy(1), ser_2=get_strategy(1))
+def test_buckaroo_hypothesis_single_row(ser_1, ser_2):
+    df = pd.DataFrame({"a": ser_1, "b":ser_2})
+    buckaroo.BuckarooWidget(df, debug=True)
