27625e7e0973d358c611ebbc8f7d9872e6a2016f
Paddy Mullen <paddy@paddymullen.com>
2023-11-26 10:20:58 -0500

    decent hypothesis test that exercises BuckarooWidget well

--- tests/unit/hypothesis_buckaroo.py
+++ tests/unit/hypothesis_buckaroo.py
@@ -6,6 +6,7 @@ from hypothesis import given, strategies as st
 from hypothesis.extra.pandas import data_frames, column
 
 
+@hypothesis.settings(deadline=10_000)
 @hypothesis.given(
     # TODO: write strategy to support n-Columns
     df=st.one_of(data_frames(columns=[column(elements=st.integers())]),
@@ -19,9 +20,7 @@ from hypothesis.extra.pandas import data_frames, column
     summaryStats=st.booleans(),
     reorderdColumns=st.booleans(),
     showCommands=st.booleans(),
-    #auto_clean=st.booleans(),
     postProcessingF=st.none(),
-    debug=st.booleans(),
 )
 def test_fuzz_BuckarooWidget(
         df,
@@ -29,18 +28,18 @@ def test_fuzz_BuckarooWidget(
         summaryStats,
         reorderdColumns,
         showCommands,
-        #auto_clean,
         postProcessingF,
-        debug,
 ):
+
+    #debug=True will cause an analysis error to throw an error,
+    #auto_clean is tempermental and not turned on by default
     buckaroo.BuckarooWidget(
         df=df,
         sampled=sampled,
         summaryStats=summaryStats,
         reorderdColumns=reorderdColumns,
         showCommands=showCommands,
-        #auto_clean=auto_clean,
         auto_clean=False,
         postProcessingF=postProcessingF,
-        debug=debug,
+        debug=True
     )
