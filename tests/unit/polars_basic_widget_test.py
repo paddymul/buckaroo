6c9108428b38d5b3a82593e9672d8629afbe399c
Paddy Mullen <paddy@paddymullen.com>
2023-12-01 11:58:14 -0500

    note to future self

--- tests/unit/polars_basic_widget_test.py
+++ tests/unit/polars_basic_widget_test.py
@@ -10,3 +10,7 @@ def test_sdf_hints():
     pbw = polars_buckaroo.PolarsBuckarooWidget(
         pl.DataFrame({'a':[1,2,3]}), auto_clean=False, debug=True)
     assert pbw.stats.sdf['a']['type'] == 'integer'
+
+'''
+FIXME:test a large dataframe that forces sampling
+'''
