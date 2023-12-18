b82fc831ffe325c428c767fa4101b579f5da90df
Paddy Mullen <paddy@paddymullen.com>
2023-12-18 09:55:09 -0500

    version bump

--- full_build.sh
+++ full_build.sh
@@ -6,5 +6,5 @@ python -m build .
 rm -f dist/index.js dist/index.js.LICENSE.txt
 twine check dist/*
 python -m twine upload --repository testpypi dist/*
-python -m twine upload --repository pypi dist/*
+#python -m twine upload --repository pypi dist/*
 #npm publish
