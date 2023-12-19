c6a31746c3e13c87e2d708c43b92686fb166ef25
Paddy Mullen <paddy@paddymullen.com>
2023-12-19 14:02:44 -0500

    refactorigns

--- js/components/gridUtils.ts
+++ js/components/gridUtils.ts
@@ -235,6 +235,26 @@ export function addToColDef(
   return undefined;
 }
 
+export function getHistoIndex(val:number, histogram_edges:number[]): number {
+  /*
+np.histogram([1, 2, 3, 4,  10, 20, 30, 40, 300, 300, 400, 500], bins=5)
+( [  8,       0,     2,     1,     1], 
+[  1. , 100.8, 200.6, 300.4, 400.2, 500. ])
+The bottom matters for us, those are the endge
+
+this means that 8 values are between 1 and 100.8  and 2 values are between 200.6 and 300.4
+  */
+  if (histogram_edges.length == 0) {
+    return 0;
+  }
+  for(let i=0; i<histogram_edges.length; i++) {
+    if (val <= histogram_edges[i]) {
+      return i;
+    }
+  }
+  return histogram_edges.length;
+}
+
 export function colorMap(mapName: string, histogram: number[]) {
   // https://colorcet.com/gallery.html#isoluminant
   // https://github.com/holoviz/colorcet/tree/main/assets/CET
--- tests/ts/gridUtils.test.ts
+++ tests/ts/gridUtils.test.ts
@@ -1,6 +1,6 @@
 //import { add, multiply } from "../src/math";
 
-import { extractSDFT } from '../../js/components/gridUtils';
+import { extractSDFT, getHistoIndex } from '../../js/components/gridUtils';
 import { DFData } from "../../js/components/DFWhole";
 
 describe("testing utility functions in gridUtils ", () => {
@@ -20,31 +20,16 @@ describe("testing utility functions in gridUtils ", () => {
     expect(result).toEqual(expected);
   });
   it("color mapper should pick from ranges properly", () => {
-    
-
-  function getHistoIndex(val:number, histogram_edges:number[]): number {
-    /*
-  np.histogram([1, 2, 3, 4,  10, 20, 30, 40, 300, 300, 400, 500], bins=5)
+     /*
+np.histogram([1, 2, 3, 4,  10, 20, 30, 40, 300, 300, 400, 500], bins=5)
 ( [  8,       0,     2,     1,     1], 
-  [  1. , 100.8, 200.6, 300.4, 400.2, 500. ])
-  The bottom matters for us, those are the endge
-  
-  this means that 8 values are between 1 and 100.8  and 2 values are between 200.6 and 300.4
-    */
-    if (histogram_edges.length == 0) {
-      return 0;
-    }
-    for(let i=0; i<histogram_edges.length; i++) {
-      if (val <= histogram_edges[i]) {
-        return i;
-      }
-    }
-    return histogram_edges.length;
-  }
+[  1. , 100.8, 200.6, 300.4, 400.2, 500. ])
+The bottom matters for us, those are the endge
+
+this means that 8 values are between 1 and 100.8  and 2 values are between 200.6 and 300.4
+  */
   const edges = [2, 4, 10, 30];
-
   expect(getHistoIndex(1,  [])).toBe(0);
-
   expect(getHistoIndex(1,  edges)).toBe(0);
   expect(getHistoIndex(3,  edges)).toBe(1);
   expect(getHistoIndex(5,  edges)).toBe(2);
@@ -52,7 +37,6 @@ describe("testing utility functions in gridUtils ", () => {
   expect(getHistoIndex(11, edges)).toBe(3);
   expect(getHistoIndex(11000, edges)).toBe(4);
 
-  
   });
 });
 
