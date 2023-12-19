e3f96fd8e5ad56096e2f658d8bb86089e1c39c11
Paddy Mullen <paddy@paddymullen.com>
2023-12-19 13:15:38 -0500

    test and implementation for picking value index inside histogram

--- tests/ts/gridUtils.test.ts
+++ tests/ts/gridUtils.test.ts
@@ -3,7 +3,8 @@
 import { extractSDFT } from '../../js/components/gridUtils';
 import { DFData } from "../../js/components/DFWhole";
 
-describe("Math functions", () => {
+describe("testing utility functions in gridUtils ", () => {
+  // mostly sanity checks to help develop gridUtils
 
   it("should convert to expected format", () => {
     const basicSDF:DFData = [
@@ -17,6 +18,41 @@ describe("Math functions", () => {
     };
     const result = extractSDFT(basicSDF);
     expect(result).toEqual(expected);
+  });
+  it("color mapper should pick from ranges properly", () => {
+    
+
+  function getHistoIndex(val:number, histogram_edges:number[]): number {
+    /*
+  np.histogram([1, 2, 3, 4,  10, 20, 30, 40, 300, 300, 400, 500], bins=5)
+( [  8,       0,     2,     1,     1], 
+  [  1. , 100.8, 200.6, 300.4, 400.2, 500. ])
+  The bottom matters for us, those are the endge
+  
+  this means that 8 values are between 1 and 100.8  and 2 values are between 200.6 and 300.4
+    */
+    if (histogram_edges.length == 0) {
+      return 0;
+    }
+    for(let i=0; i<histogram_edges.length; i++) {
+      if (val <= histogram_edges[i]) {
+        return i;
+      }
+    }
+    return histogram_edges.length;
+  }
+  const edges = [2, 4, 10, 30];
+
+  expect(getHistoIndex(1,  [])).toBe(0);
+
+  expect(getHistoIndex(1,  edges)).toBe(0);
+  expect(getHistoIndex(3,  edges)).toBe(1);
+  expect(getHistoIndex(5,  edges)).toBe(2);
+  expect(getHistoIndex(10, edges)).toBe(2);
+  expect(getHistoIndex(11, edges)).toBe(3);
+  expect(getHistoIndex(11000, edges)).toBe(4);
+
+  
   });
 });
 
