63cadb3988762403f3294132659be25e8b878e79
Paddy Mullen <paddy@paddymullen.com>
2023-12-21 09:33:03 -0500

    cycling statusbar

--- js/components/StatusBar.tsx
+++ js/components/StatusBar.tsx
@@ -6,24 +6,13 @@ import { ColDef, GridOptions } from 'ag-grid-community';
 import { basicIntFormatter } from './DFViewerParts/Displayer';
 export type setColumFunc = (newCol: string) => void;
 
-export interface DfConfig {
-  totalRows: number;
-  columns: number;
-  rowsShown: number;
-  sampleSize: number;
-  sampled: boolean;
-  summaryStats: boolean;
-  showCommands: boolean;
-  //reorderedColumns is currently disabled as a function, but it will be added back soon
-  //reorderdColumns: boolean;
-}
 
 const columnDefs: ColDef[] = [
   {
-    field: 'summaryStats',
+    field: 'summary_stats',
     headerName: 'Σ', //note the greek symbols instead of icons which require buildchain work
     headerTooltip: 'Summary Stats',
-    width: 30,
+    width: 120,
   },
   //   { field: 'reorderdColumns',
   //   headerName: "Θ",
@@ -31,7 +20,7 @@ const columnDefs: ColDef[] = [
   //   width:30
   // },
   {
-    field: 'showCommands',
+    field: 'show_commands',
     headerName: 'λ',
     headerTooltip: 'Show Commands',
     width: 30,
@@ -61,6 +50,158 @@ const columnDefs: ColDef[] = [
   { field: 'rowsShown', width: 120 },
   { field: 'sampleSize', width: 120 },
 ];
+
+
+export interface DfConfig {
+  totalRows: number;
+  columns: number;
+  rowsShown: number;
+  sampleSize: number;
+  sampled: boolean;
+  summaryStats: boolean;
+  showCommands: boolean;
+  //reorderedColumns is currently disabled as a function, but it will be added back soon
+  //reorderdColumns: boolean;
+}
+
+
+export interface DFMeta { // static, 
+  totalRows: number;
+  columns: number;
+  rowsShown: number;
+  sampleSize: number;
+}
+
+export interface BuckarooState {
+  sampled: string | false;
+  summary_stats: string| false; // there could be multiple 
+  show_commands: string | false;
+  auto_clean: string | false;
+  reorderd_columns: string | false;
+}
+
+export interface BuckarooOptions {
+  sampled: string[];
+  summary_stats: string[];
+  show_commands: string[];
+  auto_clean: string[];
+  reorderd_columns: string[];
+}
+
+export type BKeys = keyof BuckarooState;
+
+
+export function StatusBarEx() {
+
+  const dfm: DFMeta = {
+    'columns': 5,
+    'rowsShown' : 20,
+    'sampleSize' : 10_000,
+    'totalRows' : 877
+  }
+
+  const [bState, setBState] = useState<BuckarooState>({
+    'auto_clean': 'aggressive',
+    'reorderd_columns': false,
+    'sampled' : false,
+    'show_commands' : false,
+    'summary_stats' : 'typing_stats'
+  })
+
+  const bOptions: BuckarooOptions = {
+    'auto_clean': ['aggressive', 'conservative'],
+    'reorderd_columns': [],
+    'sampled' : ['random'],
+    'show_commands' : ['on'],
+    'summary_stats' : ['full', 'all', 'typing_stats']
+  }
+
+
+  return <StatusBar2 
+                      dfMeta={dfm}
+                      buckarooState={bState}
+                      setBuckarooState={setBState}
+                      buckarooOptions={bOptions}
+                      />
+}
+export function StatusBar2({
+  dfMeta,
+  buckarooState,
+  setBuckarooState,
+  buckarooOptions,
+}: {
+  dfMeta: DFMeta;
+  buckarooState:BuckarooState;
+  setBuckarooState:React.Dispatch<React.SetStateAction<BuckarooState>>;
+  buckarooOptions:BuckarooOptions;
+}) {
+
+  const rowData = [
+    {
+      totalRows: basicIntFormatter.format(dfMeta.totalRows),
+      'columns': dfMeta.columns,
+      rowsShown: basicIntFormatter.format(dfMeta.rowsShown),
+      sampleSize: basicIntFormatter.format(dfMeta.sampleSize),
+      sampled: buckarooState.sampled ||  '0',
+      summary_stats: buckarooState.summary_stats ||  '0',
+      show_commands: buckarooState.show_commands  || '0',
+    },
+  ];
+
+
+  _.map({'a':10, 'b':20}, (v, k) => {console.log("k", k, v)}) 
+  const optionCycles =  _.fromPairs(_.map(buckarooOptions, (v:any, k) => [k, _.concat([false], v)]));
+  //@ts-ignore
+  const idxs = _.fromPairs(_.map(_.keys(optionCycles), (k) => [k, _.indexOf(optionCycles[k], buckarooState[k])]))
+
+  const nextIndex = (curIdx:number, arr:any[]) => {
+    if (curIdx == (arr.length - 1)) {
+      return 0;
+    }
+    return curIdx + 1;
+  }
+
+  const newBuckarooState = (k:BKeys) => {
+    const arr = buckarooOptions[k];
+    console.log("k", k, "arr", arr)
+    const curIdx = idxs[k];
+    const nextIdx = nextIndex(curIdx, arr);
+    const newVal = arr[nextIdx];
+    const newState = _.clone(buckarooState);
+    newState[k] = newVal;
+    return newState;
+  }
+  const updateDict = (event: any) => {
+    const colName = event.column.getColId();
+    setBuckarooState(newBuckarooState( colName as BKeys));
+  };
+
+  const gridOptions: GridOptions = {
+    rowSelection: 'single',
+  };
+
+  const gridRef = useRef<AgGridReact<unknown>>(null);
+  const defaultColDef = {
+    type: 'left-aligned',
+    cellStyle: { textAlign: 'left' },
+  };
+  return (
+    <div className="statusBar">
+      <div style={{ height: 50 }} className="theme-hanger ag-theme-alpine-dark">
+        <AgGridReact
+          ref={gridRef}
+          onCellClicked={updateDict}
+          gridOptions={gridOptions}
+          defaultColDef={defaultColDef}
+          rowData={rowData}
+          columnDefs={columnDefs}
+        ></AgGridReact>
+      </div>
+    </div>
+  );
+}
+
+
 
 export function StatusBar({
   config,
@@ -128,19 +269,4 @@ export function StatusBar({
       </div>
     </div>
   );
-}
-
-export function StatusBarEx() {
-  const [sampleConfig, setConfig] = useState<DfConfig>({
-    totalRows: 1309,
-    columns: 30,
-    rowsShown: 500,
-    sampleSize: 10_000,
-    sampled: true,
-    summaryStats: false,
-    showCommands: true,
-    //reorderdColumns: true,
-  });
-
-  return <StatusBar config={sampleConfig} setConfig={setConfig} />;
 }
