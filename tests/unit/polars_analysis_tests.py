5cbf3d29f685656851caebe495ad8b70c47de4dc
Paddy Mullen <paddy@paddymullen.com>
2024-01-12 09:27:31 -0500

    should have been included long ago

---
+++ tests/unit/polars_analysis_tests.py
@@ -0,0 +1,46 @@
+from unittest import TestCase
+
+import polars as pl
+import numpy as np
+from polars import functions as F
+from buckaroo.customizations.polars_analysis import HistogramAnalysis
+
+from buckaroo.pluggable_analysis_framework.utils import (json_postfix, replace_in_dict)
+
+from buckaroo.pluggable_analysis_framework.polars_analysis_management import (
+    produce_series_df, PolarsAnalysis, extract_table_hint)
+
+test_df = pl.DataFrame({
+        'normal_int_series' : pl.Series([1,2,3,4]),
+        #'empty_na_ser' : pl.Series([pl.Null] * 4, dtype="Int64"),
+        'float_nan_ser' : pl.Series([3.5, np.nan, 4.8, 2.2])
+    })
+
+word_only_df = pl.DataFrame({'letters': 'h o r s e'.split(' ')})
+
+df = pl.read_csv('./examples/data/2014-01-citibike-tripdata.csv')
+
+empty_df = pl.DataFrame({})
+#empty_df_with_columns = pl.DataFrame({}, columns=[0])
+
+
+
+class SelectOnlyAnalysis(PolarsAnalysis):
+
+    select_clauses = [
+        F.all().null_count().name.map(json_postfix('null_count')),
+        F.all().mean().name.map(json_postfix('mean')),
+        F.all().quantile(.99).name.map(json_postfix('quin99'))]
+
+
+def test_produce_series_df():
+    """just make sure this doesn't fail"""
+    
+    sdf, errs = produce_series_df(
+        test_df, [SelectOnlyAnalysis], 'test_df', debug=True)
+    expected = {
+        'float_nan_ser':      {'mean': None, 'null_count':  0, 'quin99': None},
+        'normal_int_series':  {'mean': 2.5,  'null_count':  0, 'quin99':  4.0}}
+    dsdf = replace_in_dict(sdf, [(np.nan, None)])
+    assert dsdf == expected
+
