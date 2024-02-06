b7e8afe374bd39cffc5c9e6e97604189bc80b735
Paddy Mullen <paddy@paddymullen.com>
2024-02-06 11:21:27 -0500

    trying exception_protect for saner stacktraces

--- buckaroo/dataflow_traditional.py
+++ buckaroo/dataflow_traditional.py
@@ -80,6 +80,16 @@ def merge_column_config(styled_column_config, overide_column_configs):
     return ret_column_config
 
 import six
+def exception_protect(func):
+    def wrapped(self, *args, **kwargs):
+        try:
+            func(self, *args, **kwargs)
+        except Exception as e:
+            if self.exception is None:
+                self.exception = sys.exc_info()
+            raise
+    return wrapped
+
 class DataFlow(HasTraits):
     """This class is meant to only represent the dataflow through
     buckaroo with no accomodation for widget particulars
@@ -101,6 +111,7 @@ class DataFlow(HasTraits):
         super().__init__()
         self.summary_sd = {}
         self.existing_operations = []
+        self.exception = None
         try:
             self.raw_df = raw_df
         except Exception as e:
@@ -149,13 +160,9 @@ class DataFlow(HasTraits):
 
 
     @observe('raw_df', 'sample_method')
+    @exception_protect
     def _sampled_df(self, change):
-        try:
-            self.sampled_df = self._compute_sampled_df(self.raw_df, self.sample_method)
-        except Exception as e:
-            self.exception = sys.exc_info()
-
-            raise
+        self.sampled_df = self._compute_sampled_df(self.raw_df, self.sample_method)
 
     def _run_df_interpreter(self, df, ops):
         if len(ops) == 1:
@@ -181,6 +188,7 @@ class DataFlow(HasTraits):
         return cleaning_ops, cleaning_sd
 
     @observe('sampled_df', 'cleaning_method', 'existing_operations')
+    @exception_protect
     def _operation_result(self, change):
         if self.sampled_df is None:
             return
@@ -215,6 +223,7 @@ class DataFlow(HasTraits):
         return [cleaned_df, {}]
 
     @observe('cleaned', 'post_processing_method')
+    @exception_protect
     def _processed_result(self, change):
         #for now this is a no-op because I don't have a post_processing_function or mechanism
         self.processed_result = self._compute_processed_result(self.cleaned_df, self.post_processing_method)
@@ -244,10 +253,12 @@ class DataFlow(HasTraits):
         return ret_summary
 
     @observe('processed_result', 'analysis_klasses')
+    @exception_protect
     def _summary_sd(self, change):
         self.summary_sd = self._get_summary_sd(self.processed_df)
 
     @observe('summary_sd')
+    @exception_protect
     def _merged_sd(self, change):
         #slightly inconsitent that processed_sd gets priority over
         #summary_sd, given that processed_df is computed first. My
@@ -263,6 +274,7 @@ class DataFlow(HasTraits):
         return dfviewer_config
 
     @observe('merged_sd', 'style_method')
+    @exception_protect
     def _widget_config(self, change):
         #how to control ordering of column_config???
         # dfviewer_config = self._get_dfviewer_config(self.merged_sd, self.style_method)
