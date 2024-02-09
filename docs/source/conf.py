eda332b1bbe399da9b71e784679c6db38fdce347
Paddy Mullen <paddy@paddymullen.com>
2024-02-09 13:02:27 -0500

    version bump to 0.6.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Buckaroo'
 copyright = '2023, Paddy Mullen'
 author = 'Paddy Mullen'
-release = '0.6'
+release = '0.6.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
