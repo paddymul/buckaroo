b0f9c3e03c322ed7fa0c5bf25c608517c29c1687
Paddy Mullen <paddy@paddymullen.com>
2024-02-12 19:12:36 -0500

    merge_ops test apsses

--- buckaroo/dataflow/autocleaning.py
+++ buckaroo/dataflow/autocleaning.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from buckaroo.jlisp.lisp_utils import split_operations, lists_match
 from buckaroo.pluggable_analysis_framework.utils import (filter_analysis)
+from buckaroo.pluggable_analysis_framework.polars_analysis_management import PlDfStats, PolarsAnalysis
 from ..customizations.all_transforms import configure_buckaroo, DefaultCommandKlsList
 
 def dumb_merge_ops(existing_ops, cleaning_ops):
@@ -8,13 +10,10 @@ def dumb_merge_ops(existing_ops, cleaning_ops):
     a.extend(cleaning_ops)
     return a
 
-
 SENTINEL_DF_1 = pd.DataFrame({'foo'  :[10, 20], 'bar' : ["asdf", "iii"]})
 SENTINEL_DF_2 = pd.DataFrame({'col1' :[55, 55], 'col2': ["pppp", "333"]})
 SENTINEL_DF_3 = pd.DataFrame({'pp'   :[99, 33], 'ee':   [     6,     9]})
 SENTINEL_DF_4 = pd.DataFrame({'vvv'  :[12, 49], 'oo':   [ 'ccc', 'www']})
-
-
 
 class SentinelAutocleaning:
 
@@ -40,20 +39,30 @@ class SentinelAutocleaning:
 
 def merge_ops(existing_ops, cleaning_ops):
     """ strip cleaning_ops from existing_ops, reinsert cleaning_ops at the beginning """
-    pass
+    old_cleaning_ops, user_gen_ops = split_operations(existing_ops)
+    merged = cleaning_ops.copy()
+    merged.extend(user_gen_ops)
+    return merged
+
+
+
+def format_ops(column_meta):
+    ret_ops = []
+    for k,v in column_meta.items():
+        ops = v['cleaning_ops']
+        if len(ops) > 0:
+            temp_ops = ops.copy()
+            temp_ops.insert(1, k)
+            ret_ops.append(temp_ops)
+    return ret_ops
+
 
 class Autocleaning:
-
-
     command_klasses = [DefaultCommandKlsList]
-
     autocleaning_analysis_klasses = []
-    
 
     ### start code interpreter block
     def _setup_from_command_kls_list(self):
-
-
         #used to initially setup the interpreter, and when a command
         #is added interactively
         c_klasses = self.command_klasses
@@ -80,6 +89,23 @@ class Autocleaning:
             return 'no operations'
         return self.gencode_interpreter(operations)
 
+    def _run_cleaning(self, df, cleaning_method):
+        dfs = PlDfStats(df, self.autocleaning_analysis_klasses, debug=True)
+        gen_ops = format_ops(dfs.sdf)
+
+        cleaning_sd = {}
+        return gen_ops, cleaning_sd
+
+
+    def handle_ops_and_clean(self, df, cleaning_method, existing_operations):
+        if df is None:
+            return None
+        cleaning_operations, cleaning_sd = self._run_cleaning(df, cleaning_method)
+        merged_operations = merge_ops(existing_operations, cleaning_operations)
+        cleaned_df = self._run_df_interpreter(df, merged_operations)
+        generated_code = self._run_code_generator(merged_operations)
+        return [cleaned_df, cleaning_sd, generated_code, merged_operations]
+
     def handle_ops_and_clean_orig(self, df, cleaning_method, existing_operations):
         if self.sampled_df is None:
             return None
@@ -87,5 +113,5 @@ class Autocleaning:
         merged_operations = merge_ops(existing_operations, cleaning_operations)
         cleaned_df = self._run_df_interpreter(df, merged_operations)
         generated_code = self._run_code_generator(merged_operations)
-        self.cleaned = [cleaned_df, cleaning_sd, generated_code, merged_operations]
+        self.cleaned [cleaned_df, cleaning_sd, generated_code, merged_operations]
 
--- tests/unit/dataflow/autocleaning_test.py
+++ tests/unit/dataflow/autocleaning_test.py
@@ -3,6 +3,12 @@ from buckaroo.customizations.polars_analysis import (
     VCAnalysis, PLCleaningStats, BasicAnalysis)
 from buckaroo.pluggable_analysis_framework.polars_analysis_management import PlDfStats, PolarsAnalysis
 from buckaroo.jlisp.lisp_utils import split_operations, lists_match
+from buckaroo.dataflow.autocleaning import Autocleaning, merge_ops, format_ops
+from buckaroo.customizations.polars_commands import (
+    DropCol, FillNA, GroupBy #, OneHot, GroupBy, reindex
+)
+
+
 
 dirty_df = pl.DataFrame({
     'a':[10,  20,  30,   40,  10, 20.3,   5, None, None, None],
@@ -19,9 +25,7 @@ class CleaningGenOps(PolarsAnalysis):
     requires_summary = ['int_parse_fail', 'int_parse']
     provides_defaults = {'cleaning_ops': []}
 
-    
     int_parse_threshhold = .3
-    
     @classmethod
     def computed_summary(kls, column_metadata):
         if column_metadata['int_parse'] > kls.int_parse_threshhold:
@@ -49,18 +53,8 @@ def test_ops_gen():
     assert dfs.sdf['b']['cleaning_ops'] == []
 
 
-def format_ops(column_meta):
-    ret_ops = []
-    for k,v in column_meta.items():
-        ops = v['cleaning_ops']
-        if len(ops) > 0:
-            temp_ops = ops.copy()
-            temp_ops.insert(1, k)
-            ret_ops.append(temp_ops)
-    return ret_ops
 
 def test_format_ops():
-
     column_meta = {
         'a': {'cleaning_ops':SAFE_INT_TOKEN },
         'b': {'cleaning_ops': [{'symbol': 'replace_dirty', 'meta':{'auto_clean': True}}, '\n', None]}}
@@ -68,7 +62,49 @@ def test_format_ops():
     expected_ops = [
         [{'symbol': 'safe_int', 'meta':{'auto_clean': True}}, 'a'],
         [{'symbol': 'replace_dirty', 'meta':{'auto_clean': True}}, 'b', '\n', None]]
-
-    
     assert format_ops(column_meta) == expected_ops
 
+
+class AlwaysSafeIntGenOps(PolarsAnalysis):
+    requires_summary = []
+    provides_defaults = {'cleaning_ops': []}
+
+    @classmethod
+    def computed_summary(kls, column_metadata):
+        return {'cleaning_ops': [{'symbol': 'safe_int', 'meta':{'auto_clean': True}}]}
+
+def test_merge_ops():
+    existing_ops = [
+        [{'symbol': 'safe_int', 'meta':{'auto_clean': True}}, 'a'],
+        [{'symbol': 'usergen'}, 'foo_column']
+    ]
+
+    cleaning_ops = [
+        [{'symbol': 'new_cleaning', 'meta':{'auto_clean': True}}, 'a']]
+
+    expected_merged = [
+        [{'symbol': 'new_cleaning', 'meta':{'auto_clean': True}}, 'a'],
+        [{'symbol': 'usergen'}, 'foo_column']
+    ]
+    print( merge_ops(existing_ops, cleaning_ops))
+    print("@"*80)
+    assert merge_ops(existing_ops, cleaning_ops) == expected_merged
+        
+
+def test_handle_user_ops():
+
+    
+    class ACModded(Autocleaning):
+        autocleaning_analysis_klasses = [VCAnalysis, PLCleaningStats, BasicAnalysis, CleaningGenOps]
+        command_klasses = [DropCol, FillNA, GroupBy]
+    
+    ac = ACModded()    
+    df = pl.DataFrame({'a': [10, 20, 30]})
+
+    cleaning_result = ac.handle_ops_and_clean(df, cleaning_method='normal', existing_operations=[])
+
+    cleaned_df, cleaning_sd, generated_code, merged_operations = cleaning_result
+    
+    
+    assert merged_operations == [
+        [{'symbol': 'safe_int', 'meta':{'auto_clean': True}}, 'a']]
