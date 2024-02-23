40c59a4eec6786c695a64c46b45300936ae0a4d6
Paddy Mullen <paddy@paddymullen.com>
2024-02-23 13:44:50 -0500

    version bump (#236)
    This releases fixes for
     Add a DFViewer component #231 
     Add image displayer #228 
     SolaraBuckaroo #229 
     At least notify users that Buckaroo has changed output handler for DataFrames, show debug statement #225

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Buckaroo'
 copyright = '2023, Paddy Mullen'
 author = 'Paddy Mullen'
-release = '0.6.2'
+release = '0.6.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
