afe122809c01440ba8c17f3440081c9286d64d4a
Paddy Mullen <paddy@paddymullen.com>
2024-10-29 15:17:40 -0400

    cleaned all notebooks and added cleaning command

---
+++ scripts/clean_notebooks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#note docs/example-notebooks/Exception-Traits-demo.ipynb specifically has
+# stack traces to demonstrate stack trace behavior
+nbstripout docs/example-notebooks/DFViewer.ipynb \
+ docs/example-notebooks/Extending-pandas.ipynb \
+ docs/example-notebooks/Extending.ipynb \
+ docs/example-notebooks/Filter.ipynb \
+ docs/example-notebooks/Full-tour.ipynb \
+ docs/example-notebooks/GeoPandas.ipynb \
+ docs/example-notebooks/Histograms-demo.ipynb \
+ docs/example-notebooks/Itables-testcases.ipynb \
+ docs/example-notebooks/Pluggable-Analysis-Framework.ipynb \
+ docs/example-notebooks/Solara-Buckaroo.ipynb \
+ docs/example-notebooks/introduction.ipynb \
+ docs/example-notebooks/styling-gallery.ipynb \
+ docs/example-notebooks/styling-howto.ipynb \
+ docs/example-notebooks/testcases-fast.ipynb
