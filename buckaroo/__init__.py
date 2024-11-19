f0219b62aaa73eb5b9def2ec5ecc5b6a342fd7b5
Paddy Mullen <paddy@paddymullen.com>
2024-11-19 16:34:58 -0500

    updated version warning message, remvoed unused cruft

--- buckaroo/__init__.py
+++ buckaroo/__init__.py
@@ -5,56 +5,13 @@ from .dataflow.widget_extension_utils import DFViewer
 from .widget_utils import is_in_ipython, enable, disable, determine_jupter_env
 
 
-def _jupyter_nbextension_paths():
-    """Called by Jupyter Notebook Server to detect if it is a valid nbextension and
-    to install the widget
-    Returns
-    =======
-    section: The section of the Jupyter Notebook Server to change.
-        Must be 'notebook' for widget extensions
-    src: Source directory name to copy files from. Webpack outputs generated files
-        into this directory and Jupyter Notebook copies from this directory during
-        widget installation
-    dest: Destination directory name to install widget files to. Jupyter Notebook copies
-        from `src` directory into <jupyter path>/nbextensions/<dest> directory
-        during widget installation
-    require: Path to importable AMD Javascript module inside the
-        <jupyter path>/nbextensions/<dest> directory
-    """
-    return [{
-        'section': 'notebook',
-        'src': 'nbextension',
-        'dest': "buckaroo",
-        'require': 'buckaroo/extension'
-    }]
-
-
-def _jupyter_labextension_paths():
-    """Called by Jupyter Lab Server to detect if it is a valid labextension and
-    to install the widget
-    Returns
-    =======
-    src: Source directory name to copy files from. Webpack outputs generated files
-        into this directory and Jupyter Lab copies from this directory during
-        widget installation
-    dest: Destination directory name to install widget files to. Jupyter Lab copies
-        from `src` directory into <jupyter path>/labextensions/<dest> directory
-        during widget installation
-    """
-    return [{
-        'src': 'labextension',
-        'dest': "buckaroo"
-    }]
-
-
-
 
 def is_notebook_compatible():
     jupyter_env = determine_jupter_env()
     if jupyter_env == "jupyter-notebook":
         try:
             import notebook
-            return notebook.version_info[0] >= 7
+            return notebook.version_info[0] >= 6
         except:
             pass
         return False
@@ -64,7 +21,7 @@ def is_notebook_compatible():
 def warn_on_incompatible():
     if not is_notebook_compatible():
         import notebook
-        print("Buckaroo is compatible with jupyter notebook > 7, or jupyterlab >3.6.0")
+        print("Buckaroo is compatible with jupyter notebook > 6, or jupyterlab >3.6.0")
         print("You seem to be executing this in jupyter notebook version %r" % str(notebook.__version__))
         print("You can upgrade to notebook 7 by running 'pip install --upgrade notebook'")
         print("Or you can try running jupyter lab with 'jupyter lab'")
