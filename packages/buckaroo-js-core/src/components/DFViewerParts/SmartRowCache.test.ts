c7fa53594adc181f9efab23d671cf2099c9f04bc
Paddy Mullen <paddy@paddymullen.com>
2025-02-03 16:58:02 -0500

    test actually runs

--- packages/buckaroo-js-core/src/components/DFViewerParts/SmartRowCache.test.ts
+++ packages/buckaroo-js-core/src/components/DFViewerParts/SmartRowCache.test.ts
@@ -1,15 +1,29 @@
 //import { describe, expect } from 'jest';
 import {
     Segment,
-    segmentLT } from "./SmartRowCache"
+    segmentLT,
+    segmentsOverlap
+} from "./SmartRowCache"
 
 
+const low:Segment = [20,30]
+const mid:Segment = [ 25, 55];
+const high:Segment = [50, 100]
+const around:Segment = [20,120]
 describe('foo', () => {
     test('test segmentLT', () => {
-	const high:Segment = [50, 100]
-	const low:Segment = [20,30]
 	expect(segmentLT(low, high)).toBe(true);
 	expect(segmentLT(high, low)).toBe(false);
 	expect(segmentLT(high, high)).toBe(false);
+	expect(segmentLT(around, high)).toBe(false);
+    });
+    test('test segmentsOverlap', () => {
+	expect(segmentsOverlap(low, high)).toBe(false);
+	expect(segmentsOverlap(high, low)).toBe(false);
+	expect(segmentsOverlap(high, high)).toBe(true);
+	expect(segmentsOverlap(around, high)).toBe(true);
+
+	expect(segmentsOverlap(mid, high)).toBe(true);
+	expect(segmentsOverlap(low, mid)).toBe(true);
     });
 })
