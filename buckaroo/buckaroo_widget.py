d006dfbeacfe06b2e0eaea8f556ce752768198fc
Paddy Mullen <paddy@paddymullen.com>
2025-02-13 13:01:04 -0500

    Working infinite widget with send/on_msg

--- buckaroo/buckaroo_widget.py
+++ buckaroo/buckaroo_widget.py
@@ -329,29 +329,23 @@ class BuckarooInfiniteWidget(BuckarooWidget):
                             'data': []}
                             ).tag(sync=True)
 
-    #    @exception_protect('payloadArgsHandler')
-
-
     def __init__(self, orig_df, debug=False,
                  column_config_overrides=None,
                  pinned_rows=None, extra_grid_config=None,
                  component_config=None, init_sd=None):
         super().__init__(orig_df, debug, column_config_overrides, pinned_rows,
-                         extra_grid_config, component_config, init_sd)
+                     extra_grid_config, component_config, init_sd)
 
         def payload_bridge(_unused_self, msg, _unused_buffers):
             print("payload_bridge")
             print(msg)
             print("-"*80)
             print(_unused_buffers)
-            import pdb
-            pdb.set_trace()
             if msg['type'] == 'infinite_request':
                 payload_args = msg['payload_args']
-                self._handle_widget_change(payload_args)
+                self._handle_payload_args(payload_args)
 
         self.on_msg(payload_bridge)
-
 
     def _handle_payload_args(self, new_payload_args):
         start, end = new_payload_args['start'], new_payload_args['end']
@@ -371,7 +365,7 @@ class BuckarooInfiniteWidget(BuckarooWidget):
             else:
                 slice_df = pd_to_obj(processed_df[start:end])
                 self.send({ "type": "infinite_resp", 'key':new_payload_args, 'data':slice_df, 'length':len(processed_df)})
-
+    
                 extra_start, extra_end = end, end+100
                 extra_payload_args = dict(
                     start=extra_start, end=extra_end,
@@ -382,8 +376,8 @@ class BuckarooInfiniteWidget(BuckarooWidget):
             print(e)
             stack_trace = traceback.format_exc()
             self.send({ "type": "infinite_resp", 'key':new_payload_args, 'data':[], 'error_info':stack_trace, 'length':0})
-           
             raise
+
 
         
     @observe('payload_args')
@@ -394,61 +388,3 @@ class BuckarooInfiniteWidget(BuckarooWidget):
     def _df_to_obj(self, df:pd.DataFrame):
         return pd_to_obj(df)
 
-
-'''
-from buckaroo.serialization_utils import EMPTY_DF_WHOLE, check_and_fix_df, pd_to_obj
-class TBW(BuckarooInfiniteWidget):
-        def __init__(self, orig_df, debug=False,
-                 column_config_overrides=None,
-                 pinned_rows=None, extra_grid_config=None,
-                 component_config=None, init_sd=None):
-            super().__init__(orig_df, debug, column_config_overrides, pinned_rows,
-                         extra_grid_config, component_config, init_sd)
-
-            def payload_bridge(_unused_self, msg, _unused_buffers):
-                print("payload_bridge")
-                print(msg)
-                print("-"*80)
-                print(_unused_buffers)
-                #import pdb
-                #pdb.set_trace()
-                if msg['type'] == 'infinite_request':
-                    payload_args = msg['payload_args']
-                    self._handle_payload_args(payload_args)
-    
-            self.on_msg(payload_bridge)
-        def _handle_payload_args(self, new_payload_args):
-            start, end = new_payload_args['start'], new_payload_args['end']
-            print("payload_args changed", start, end)
-            _unused, processed_df, merged_sd = self.widget_args_tuple
-            if processed_df is None:
-                return
-
-            try:
-                sort = new_payload_args.get('sort')
-                if sort:
-                    sort_dir = new_payload_args.get('sort_direction')
-                    ascending = sort_dir == 'asc'
-                    sorted_df = processed_df.sort_values(by=[sort], ascending=ascending)
-                    slice_df = pd_to_obj(sorted_df[start:end])
-                    self.send({ "type": "infinite_resp", 'key':new_payload_args, 'data':slice_df, 'length':len(processed_df)})
-                else:
-                    slice_df = pd_to_obj(processed_df[start:end])
-                    self.send({ "type": "infinite_resp", 'key':new_payload_args, 'data':slice_df, 'length':len(processed_df)})
-        
-                    extra_start, extra_end = end, end+100
-                    extra_payload_args = dict(
-                        start=extra_start, end=extra_end,
-                        sourceName=new_payload_args.get('sourceName', 'no_source_name'))
-                    extra_df = pd_to_obj(processed_df[extra_start:extra_end])
-                    #self.send({ "type": "infinite_resp", 'key':new_payload_args, 'data':extra_df, 'length':len(processed_df)})
-            except Exception as e:
-                print(e)
-                stack_trace = traceback.format_exc()
-                self.send({ "type": "infinite_resp", 'key':new_payload_args, 'data':[], 'error_info':stack_trace, 'length':0})
-                raise
-
-TBW(typed_df)
-
-the above worked in a notebook, getting it into git
-'''
