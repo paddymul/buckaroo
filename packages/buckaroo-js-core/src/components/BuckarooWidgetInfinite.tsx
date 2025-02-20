f1c54cfe1178944135d03d2bc3038e243c7c8b17
Paddy Mullen <paddy@paddymullen.com>
2025-02-20 17:18:04 -0500

    release 0.8.8

--- packages/buckaroo-js-core/src/components/BuckarooWidgetInfinite.tsx
+++ packages/buckaroo-js-core/src/components/BuckarooWidgetInfinite.tsx
@@ -107,7 +107,7 @@ export function BuckarooInfiniteWidget({
     return (
         <div className="dcf-root flex flex-col" style={{ width: "100%", height: "100%" }}>
             <div
-                className="orig-df flex flex-row"
+                className="orig-df"
                 style={{
                     // height: '450px',
                     overflow: "hidden",
