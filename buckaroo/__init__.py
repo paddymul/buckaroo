59523c79e13cf452671d6fa34283aef1e97511b4
Paddy Mullen <paddy@paddymullen.com>
2025-03-13 13:24:30 -0400

    quiet import in marimo.  release 0.9.7

--- buckaroo/__init__.py
+++ buckaroo/__init__.py
@@ -70,6 +70,10 @@ try:
     if is_in_ipython():
         enable()
         print("Buckaroo has been enabled as the default DataFrame viewer.  To return to default dataframe visualization use `from buckaroo import disable; disable()`")
+    if is_in_marimo():
+        # you can't register handlers in Marimo, I'm not sure what messaging to do around this.
+        pass
+    
     else:
         print("must be running inside ipython to enable default display via enable()")
 
