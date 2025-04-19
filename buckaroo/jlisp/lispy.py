0e995bdd8f3133a65c9de27914b340fa7bd7f61c
Paddy Mullen <paddy@paddymullen.com>
2025-04-19 16:05:15 -0400

    deleting dead code

--- buckaroo/jlisp/lispy.py
+++ buckaroo/jlisp/lispy.py
@@ -105,46 +105,26 @@ def callcc(proc):
         else: raise w
 isa = isinstance
 
-def add_globals(env):
+
+def add_globals(self):
     "Add some Scheme standard procedures."
     import math, cmath, operator as op
-    env.update(vars(math))
-    env.update(vars(cmath))
-    env.update({
-     '+':op.add, '-':op.sub, '*':op.mul, '/':op.truediv,'/':op.floordiv,
-     'not':op.not_, '>':op.gt, '<':op.lt, '>=':op.ge, '<=':op.le, '=':op.eq, 
+    self.update(vars(math))
+    self.update(vars(cmath))
+    self.update({
      'equal?':op.eq, 'eq?':op.is_, 'length':len, 'cons':cons,
      'car':lambda x:x[0], 'cdr':lambda x:x[1:], 'append':op.add,  
      'list':lambda *x:list(x), 'list?': lambda x:isa(x,list),
      'null?':lambda x:x==[], 'symbol?':lambda x: isa(x, Symbol),
      'boolean?':lambda x: isa(x, bool), 'pair?':is_pair, 
-      '+':op.add, '-':op.sub, '*':op.mul, '/':op.truediv, 'not':op.not_, 
-      '>':op.gt, '<':op.lt, '>=':op.ge, '<=':op.le, '=':op.eq, 
-
-     #'display':lambda x,port=sys.stdout:port.write(x if isa(x,str) else to_string(x))})
-     #'display':display
-    })
-    return env
-
-# def add_globals(self):
-#     "Add some Scheme standard procedures."
-#     import math, cmath, operator as op
-#     self.update(vars(math))
-#     self.update(vars(cmath))
-#     self.update({
-#      'equal?':op.eq, 'eq?':op.is_, 'length':len, 'cons':cons,
-#      'car':lambda x:x[0], 'cdr':lambda x:x[1:], 'append':op.add,  
-#      'list':lambda *x:list(x), 'list?': lambda x:isa(x,list),
-#      'null?':lambda x:x==[], 'symbol?':lambda x: isa(x, Symbol),
-#      'boolean?':lambda x: isa(x, bool), 'pair?':is_pair, 
-#      'port?': lambda x:isa(x,file), 'apply':lambda proc,l: proc(*l), 
-#      'eval':lambda x: eval(expand(x)), 'load':lambda fn: load(fn), 'call/cc':callcc,
-#      'open-input-file':open,'close-input-port':lambda p: p.file.close(), 
-#      'open-output-file':lambda f:open(f,'w'), 'close-output-port':lambda p: p.close(),
-#      'eof-object?':lambda x:x is eof_object, 'read-char':readchar,
-#      'read':read, 'write':lambda x,port=sys.stdout:port.write(to_string(x)),
-#      'display':lambda x,port=sys.stdout:port.write(x if isa(x,str) else to_string(x))})
-#     return self
+     'port?': lambda x:isa(x,file), 'apply':lambda proc,l: proc(*l), 
+     'eval':lambda x: eval(expand(x)), 'load':lambda fn: load(fn), 'call/cc':callcc,
+     'open-input-file':open,'close-input-port':lambda p: p.file.close(), 
+     'open-output-file':lambda f:open(f,'w'), 'close-output-port':lambda p: p.close(),
+     'eof-object?':lambda x:x is eof_object, 'read-char':readchar,
+     'read':read, 'write':lambda x,port=sys.stdout:port.write(to_string(x)),
+     'display':lambda x,port=sys.stdout:port.write(x if isa(x,str) else to_string(x))})
+    return self
 
 
 
