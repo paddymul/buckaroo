fc6d5dcacf5d5f5057efc4fc2094512152c9eca8
Paddy Mullen <paddy@paddymullen.com>
2025-04-28 17:05:00 -0400

    WIP

---
+++ buckaroo/customizations/pandas_cleaning_commands.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import re
+from buckaroo.jlisp.lisp_utils import s
+from buckaroo.customizations.pandas_commands import (Command)
+
+class IntParse(Command):
+    command_default = [s('regular_int_parse'), s('df'), "col"]
+    command_pattern = []
+
+    @staticmethod 
+    def transform(df, col):
+        df[col] = df[col].apply(pd.to_numeric, errors='coerce')
+        return df
+
+    @staticmethod 
+    def transform_to_py(df, col):
+        return "    df['%s'] = df['%s'].apply(pd.to_numeric, errors='coerce')" % (col, col)
+
+class StripIntParse(Command):
+    command_default = [s('strip_int_parse'), s('df'), "col"]
+    command_pattern = []
+
+    @staticmethod 
+    def transform(df, col):
+        _digits_and_period = re.compile(r'[^\d\.]')
+        _ser = df[col]
+        _reg_parse = _ser.apply(pd.to_numeric, errors='coerce')
+        _strip_parse = _ser.str.replace(_digits_and_period, "", regex=True).apply(pd.to_numeric, errors='coerce')
+        _combined = _reg_parse.fillna(_strip_parse)
+        df[col] = _combined
+        return df
+
+    @staticmethod 
+    def transform_to_py(df, col):
+        return f"""    _digits_and_period = re.compile(r'[^\\d\\.]')
+    _ser = df['{col}']
+    _reg_parse = _ser.apply(pd.to_numeric, errors='coerce')
+    _strip_parse = _ser.str.replace(_digits_and_period, "", regex=True).apply(pd.to_numeric, errors='coerce')
+    _combined = reg_parse.fillna(_strip_parse)
+    df['{col}'] = _combined"""
+
+
+class StrBool(Command):
+    command_default = [s('str_bool'), s('df'), "col"]
+    command_pattern = []
+
+    @staticmethod 
+    def transform(df, col):
+        TRUE_SYNONYMS = ['true', 'yes', 'on', '1']
+        FALSE_SYNONYMS = ['false', 'no', 'off', '0']
+        _ser = df[col]
+        _int_sanitize = _ser.replace(1, True).replace(0, False) 
+        _real_bools = _int_sanitize.isin([True, False])
+        _boolean_ser = _int_sanitize.where(_real_bools, pd.NA).astype('boolean')    
+        _trues = _ser.str.lower().isin(TRUE_SYNONYMS).replace(False, pd.NA).astype('boolean')
+        _falses =  ~ (_ser.str.lower().isin(FALSE_SYNONYMS).replace(False, pd.NA)).astype('boolean')
+        _combined = _boolean_ser.fillna(_trues).fillna(_falses)    
+        df[col] = _combined
+        return df
+
+    @staticmethod 
+    def transform_to_py(df, col):
+        return f"""    TRUE_SYNONYMS = ['true', 'yes', 'on', '1']
+    FALSE_SYNONYMS = ['false', 'no', 'off', '0']
+    _ser = df['{col}']
+    _int_sanitize = _ser.replace(1, True).replace(0, False) 
+    _real_bools = _int_sanitize.isin([True, False])
+    _boolean_ser = _int_sanitize.where(_real_bools, pd.NA).astype('boolean')    
+    _trues = _ser.str.lower().isin(TRUE_SYNONYMS).replace(False, pd.NA).astype('boolean')
+    _falses =  ~ (_ser.str.lower().isin(FALSE_SYNONYMS).replace(False, pd.NA)).astype('boolean')
+    _combined = _boolean_ser.fillna(_trues).fillna(_falses)    
+    df[col] = _combined"""
+
+
+
+
+class USDate(Command):
+    command_default = [s('us_date'), s('df'), "col"]
+    command_pattern = []
+
+    @staticmethod 
+    def transform(df, col):
+        df[col] = pd.to_datetime(df[col], errors='coerce', format="%m/%d/%Y")    
+        return df
+        
+    @staticmethod 
+    def transform_to_py(df, col):
+        return f"    df['{col}'] = pd.to_datetime(df['{col}'], errors='coerce', format='%m/%d/%Y')"
--- tests/unit/dataflow/autocleaning_heuristic_pd_test.py
+++ tests/unit/dataflow/autocleaning_heuristic_pd_test.py
@@ -5,11 +5,11 @@ from buckaroo.pluggable_analysis_framework.pluggable_analysis_framework import (
 from buckaroo.dataflow.autocleaning import AutocleaningConfig
 from buckaroo.dataflow.autocleaning import PandasAutocleaning
 from buckaroo.customizations.pandas_commands import (
-    Command,
-    SafeInt, DropCol, FillNA, GroupBy, NoOp, Search
-)
+    SafeInt, DropCol, FillNA, GroupBy, NoOp, Search)
+from buckaroo.customizations.pandas_cleaning_commands import (
+    IntParse, StripIntParse, StrBool, USDate)
 from buckaroo.customizations.pd_autoclean_conf import (NoCleaningConf)
-from buckaroo.auto_clean.heuristic_lang import eval_heuristic_rule, eval_heuristics, get_top_score
+from buckaroo.auto_clean.heuristic_lang import get_top_score
 from buckaroo.jlisp.lisp_utils import s
 import re
 
@@ -57,8 +57,6 @@ def test_autoclean_codegen():
     cleaned_df, cleaning_sd, generated_code, merged_operations = cleaning_result
 
     assert generated_code == EXPECTED_GEN_CODE
-
-
 
 
 def int_parse_frac(ser):
@@ -150,89 +148,6 @@ class HeuristicCleaningGenOps(ColAnalysis):
 
             return {'cleaning_ops':ops, 'add_orig': True}
 
-class IntParse(Command):
-    command_default = [s('regular_int_parse'), s('df'), "col"]
-    command_pattern = []
-
-    @staticmethod 
-    def transform(df, col):
-        df[col] = df[col].apply(pd.to_numeric, errors='coerce')
-        return df
-
-    @staticmethod 
-    def transform_to_py(df, col):
-        return "    df['%s'] = df['%s'].apply(pd.to_numeric, errors='coerce')" % (col, col)
-
-class StripIntParse(Command):
-    command_default = [s('strip_int_parse'), s('df'), "col"]
-    command_pattern = []
-
-    @staticmethod 
-    def transform(df, col):
-        _digits_and_period = re.compile(r'[^\d\.]')
-        _ser = df[col]
-        _reg_parse = _ser.apply(pd.to_numeric, errors='coerce')
-        _strip_parse = _ser.str.replace(_digits_and_period, "", regex=True).apply(pd.to_numeric, errors='coerce')
-        _combined = reg_parse.fillna(_strip_parse)
-        df[col] = _combined
-        return df
-
-    @staticmethod 
-    def transform_to_py(df, col):
-        return f"""    _digits_and_period = re.compile(r'[^\\d\\.]')
-    _ser = df['{col}']
-    _reg_parse = _ser.apply(pd.to_numeric, errors='coerce')
-    _strip_parse = _ser.str.replace(_digits_and_period, "", regex=True).apply(pd.to_numeric, errors='coerce')
-    _combined = reg_parse.fillna(_strip_parse)
-    df['{col}'] = _combined"""
-
-
-class StrBool(Command):
-    command_default = [s('str_bool'), s('df'), "col"]
-    command_pattern = []
-
-    @staticmethod 
-    def transform(df, col):
-        TRUE_SYNONYMS = ['true', 'yes', 'on', '1']
-        FALSE_SYNONYMS = ['false', 'no', 'off', '0']
-        _ser = df[col]
-        _int_sanitize = _ser.replace(1, True).replace(0, False) 
-        _real_bools = _int_sanitize.isin([True, False])
-        _boolean_ser = _int_sanitize.where(_real_bools, pd.NA).astype('boolean')    
-        _trues = _ser.str.lower().isin(TRUE_SYNONYMS).replace(False, pd.NA).astype('boolean')
-        _falses =  ~ (_ser.str.lower().isin(FALSE_SYNONYMS).replace(False, pd.NA)).astype('boolean')
-        _combined = _boolean_ser.fillna(_trues).fillna(_falses)    
-        df[col] = _combined
-        return df
-
-    @staticmethod 
-    def transform_to_py(df, col):
-        return f"""    TRUE_SYNONYMS = ['true', 'yes', 'on', '1']
-    FALSE_SYNONYMS = ['false', 'no', 'off', '0']
-    _ser = df['{col}']
-    _int_sanitize = _ser.replace(1, True).replace(0, False) 
-    _real_bools = _int_sanitize.isin([True, False])
-    _boolean_ser = _int_sanitize.where(_real_bools, pd.NA).astype('boolean')    
-    _trues = _ser.str.lower().isin(TRUE_SYNONYMS).replace(False, pd.NA).astype('boolean')
-    _falses =  ~ (_ser.str.lower().isin(FALSE_SYNONYMS).replace(False, pd.NA)).astype('boolean')
-    _combined = _boolean_ser.fillna(_trues).fillna(_falses)    
-    df[col] = _combined"""
-
-
-
-
-class USDate(Command):
-    command_default = [s('us_date'), s('df'), "col"]
-    command_pattern = []
-
-    @staticmethod 
-    def transform(df, col):
-        df[col] = pd.to_datetime(df[col], errors='coerce', format="%m/%d/%Y")    
-        return df
-        
-    @staticmethod 
-    def transform_to_py(df, col):
-        return f"    df['{col}'] = pd.to_datetime(df['{col}'], errors='coerce', format='%m/%d/%Y')"
 
 class ACHeuristic(AutocleaningConfig):
     """
