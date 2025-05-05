c85af987feaa9c229e7f6995d23c15b7cd1223ab
Paddy Mullen <paddy@paddymullen.com>
2025-05-05 11:34:15 -0400

    wip

--- docs/example-notebooks/mo-autocleaning.py
+++ docs/example-notebooks/mo-autocleaning.py
@@ -6,7 +6,7 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _(ACBuckaroo, pd):
-    extra_rows = 1
+    extra_rows = 100
     dirty_df = pd.DataFrame(
         {'a':[10,  20,  30,   40,  10, 20.3, None,] * extra_rows,
          'b':["3", "4", "a", "5", "5",  "b9", None ] * extra_rows,
@@ -16,6 +16,37 @@ def _(ACBuckaroo, pd):
     bw2 = ACBuckaroo(dirty_df)
     bw2
     return bw2, dirty_df, extra_rows
+
+
+@app.cell
+def _(pd, re):
+    def clean(df):
+        _digits_and_period = re.compile(r'[^\d\.]')
+        _ser = df['b']
+        _reg_parse = _ser.apply(pd.to_numeric, errors='coerce')
+        _strip_parse = _ser.str.replace(_digits_and_period, "", regex=True).apply(pd.to_numeric, errors='coerce', dtype_backend='pyarrow')
+        _combined = _reg_parse.fillna(_strip_parse)
+        df['b'] = _combined
+        df['us_dates'] = pd.to_datetime(df['us_dates'], errors='coerce', format='%m/%d/%Y')
+        TRUE_SYNONYMS = ['true', 'yes', 'on', '1']
+        FALSE_SYNONYMS = ['false', 'no', 'off', '0']
+        _ser = df['mostly_bool']
+        _int_sanitize = _ser.replace(1, True).replace(0, False) 
+        _real_bools = _int_sanitize.isin([True, False])
+        _boolean_ser = _int_sanitize.where(_real_bools, pd.NA).astype('boolean')    
+        _trues = _ser.str.lower().isin(TRUE_SYNONYMS).replace(False, pd.NA).astype('boolean')
+        _falses =  ~ (_ser.str.lower().isin(FALSE_SYNONYMS).replace(False, pd.NA)).astype('boolean')
+        _combined = _boolean_ser.fillna(_trues).fillna(_falses)    
+        df['mostly_bool'] = _combined
+        return df
+
+    return (clean,)
+
+
+@app.cell
+def _(clean, dirty_df):
+    clean(dirty_df)
+    return
 
 
 @app.cell
@@ -85,7 +116,7 @@ def _(
         autoclean_conf = tuple([NoCleaningConf, AggressiveAC, ConservativeAC])
 
         def _handle_payload_args(self, new_payload_args):
-            time.sleep(3)
+            #time.sleep(3)
             super()._handle_payload_args(new_payload_args)
     return (
         ACBuckaroo,
