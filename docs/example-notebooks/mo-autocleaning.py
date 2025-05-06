41dcd8699f6a687672dcc7f8cf1b429d4fda2fd2
Paddy Mullen <paddy@paddymullen.com>
2025-05-06 15:24:17 -0400

    adding introductory text

--- docs/example-notebooks/mo-autocleaning.py
+++ docs/example-notebooks/mo-autocleaning.py
@@ -5,14 +5,80 @@ app = marimo.App(width="medium")
 
 
 @app.cell
+def _(mo):
+    mo.md(
+        r"""
+        # Autocleaning with Buckaroo
+
+        Dealing with dirty data accounts for a large portion of the time in doing data work. We know what good data looks like, and we know the individual pandas commands to clean columns. But we have to type the same commands over and over again.
+
+        Buckaroo separates data cleaning into three steps
+        1. look at the original data with your eyes
+        2. Cycle through different cleaning approaches
+        3. Approve which cleaned columns you like
+        4. Optionally take the generated code and use it as a function
+
+        Buckaroo performs these tasks interactively and quickly because it uses heuristics, not an LLM.
+
+        All of this is customizeable.
+
+        """
+    )
+    return
+
+
+@app.cell
 def _(ACBuckaroo, pd):
     dirty_df = pd.DataFrame(
-        {'a':[10,  20,  30,   40,  10, 20.3,  None,    8,  9, 10, 11, 20, None],
-         'b':["3", "4", "a", "5", "5",  "b9", None, " 9",  "9-", 11, "867-5309", "-9", None],
-        'us_dates': ["", "07/10/1982", "07/15/1982", "7/10/1982", "17/10/1982", "03/04/1982", "03/02/2002", "12/09/1968",
-                      "03/04/1982", "", "06/22/2024", "07/4/1776", "07/20/1969"],
-         "mostly_bool": [True, "True", "Yes", "On", "false", False, "1", "Off", "0", " 0", "No", 1, None]
-        })
+        {
+            "a": [10, 20, 30, 40, 10, 20.3, None, 8, 9, 10, 11, 20, None],
+            "b": [
+                "3",
+                "4",
+                "a",
+                "5",
+                "5",
+                "b9",
+                None,
+                " 9",
+                "9-",
+                11,
+                "867-5309",
+                "-9",
+                None,
+            ],
+            "us_dates": [
+                "",
+                "07/10/1982",
+                "07/15/1982",
+                "7/10/1982",
+                "17/10/1982",
+                "03/04/1982",
+                "03/02/2002",
+                "12/09/1968",
+                "03/04/1982",
+                "",
+                "06/22/2024",
+                "07/4/1776",
+                "07/20/1969",
+            ],
+            "mostly_bool": [
+                True,
+                "True",
+                "Yes",
+                "On",
+                "false",
+                False,
+                "1",
+                "Off",
+                "0",
+                " 0",
+                "No",
+                1,
+                None,
+            ],
+        }
+    )
     ACBuckaroo(dirty_df)
     return (dirty_df,)
 
@@ -21,27 +87,39 @@ def _(ACBuckaroo, pd):
 def _(pd, re, s):
     from buckaroo.customizations.pandas_commands import (
         Command,
-        SafeInt, DropCol, FillNA, GroupBy, NoOp, Search)
+        SafeInt,
+        DropCol,
+        FillNA,
+        GroupBy,
+        NoOp,
+        Search,
+    )
     from buckaroo.customizations.pandas_cleaning_commands import (
-        IntParse, StrBool, USDate)
-    from buckaroo.customizations.pd_autoclean_conf import (NoCleaningConf)
+        IntParse,
+        StrBool,
+        USDate,
+    )
+    from buckaroo.customizations.pd_autoclean_conf import NoCleaningConf
     from buckaroo.dataflow.autocleaning import AutocleaningConfig
 
+
     class StripIntParse(Command):
-        command_default = [s('strip_int_parse'), s('df'), "col"]
+        command_default = [s("strip_int_parse"), s("df"), "col"]
         command_pattern = []
 
-        @staticmethod 
+        @staticmethod
         def transform(df, col):
-            _digits_and_period = re.compile(r'[^\d\.]')
+            _digits_and_period = re.compile(r"[^\d\.]")
             _ser = df[col]
-            _reg_parse = _ser.apply(pd.to_numeric, errors='coerce')
-            _strip_parse = _ser.str.replace(_digits_and_period, "", regex=True).apply(pd.to_numeric, errors='coerce', dtype_backend='pyarrow')
+            _reg_parse = _ser.apply(pd.to_numeric, errors="coerce")
+            _strip_parse = _ser.str.replace(
+                _digits_and_period, "", regex=True
+            ).apply(pd.to_numeric, errors="coerce", dtype_backend="pyarrow")
             _combined = _reg_parse.fillna(_strip_parse)
             df[col] = _combined
             return df
 
-        @staticmethod 
+        @staticmethod
         def transform_to_py(df, col):
             return f"""    _digits_and_period = re.compile(r'[^\\d\\.]')
         _ser = df['{col}']
@@ -73,10 +151,14 @@ def _():
     import buckaroo
     from buckaroo import BuckarooInfiniteWidget
     import numpy as np
-    from buckaroo.marimo_utils import marimo_monkeypatch, BuckarooDataFrame as DataFrame
+    from buckaroo.marimo_utils import (
+        marimo_monkeypatch,
+        BuckarooDataFrame as DataFrame,
+    )
     from buckaroo.styling_helpers import obj_, float_, pinned_histogram
     from buckaroo.extension_utils import copy_extend
     from buckaroo.customizations.styling import DefaultMainStyling
+
     # this overrides pd.read_csv and pd.read_parquet to return BuckarooDataFrames which overrides displays as BuckarooWidget, not the default marimo table
     marimo_monkeypatch()
     return (
@@ -98,23 +180,30 @@ def _():
 @app.cell
 def _(np, pd):
     import re
-    from buckaroo.pluggable_analysis_framework.pluggable_analysis_framework import (ColAnalysis)
-    def int_parse_frac(ser):
-        null_count =  (~ ser.apply(pd.to_numeric, errors='coerce').isnull()).sum()
-        return  null_count / len(ser)
+    from buckaroo.pluggable_analysis_framework.pluggable_analysis_framework import (
+        ColAnalysis,
+    )
 
-    digits_and_period = re.compile(r'[^\d\.]')
+
+    def int_parse_frac(ser):
+        null_count = (~ser.apply(pd.to_numeric, errors="coerce").isnull()).sum()
+        return null_count / len(ser)
+
+
+    digits_and_period = re.compile(r"[^\d\.]")
+
+
     def strip_int_parse_frac(ser):
         if pd.api.types.is_object_dtype(ser):
-            ser = ser.astype('string')
+            ser = ser.astype("string")
         if not pd.api.types.is_string_dtype(ser):
             return 0
         ser = ser.sample(np.min([300, len(ser)]))
         stripped = ser.str.replace(digits_and_period, "", regex=True)
 
-        #don't like the string conversion here, should still be vectorized
-        int_parsable = ser.astype(str).str.isdigit() 
-        parsable = (int_parsable | (stripped != ""))
+        # don't like the string conversion here, should still be vectorized
+        int_parsable = ser.astype(str).str.isdigit()
+        parsable = int_parsable | (stripped != "")
         return parsable.sum() / len(ser)
 
 
@@ -122,37 +211,49 @@ def _(np, pd):
     FALSE_SYNONYMS = ["false", "no", "off", "0"]
     BOOL_SYNONYMS = TRUE_SYNONYMS + FALSE_SYNONYMS
 
+
     def str_bool_frac(ser):
         ser = ser.sample(np.min([300, len(ser)]))
         if pd.api.types.is_object_dtype(ser):
-            ser = ser.astype('string')
+            ser = ser.astype("string")
         if not pd.api.types.is_string_dtype(ser):
             return 0
         matches = ser.str.lower().str.strip().isin(BOOL_SYNONYMS)
         return matches.sum() / len(ser)
 
+
     def us_dates_frac(ser):
-        parsed_dates = pd.to_datetime(ser, errors='coerce', format="%m/%d/%Y")
-        return (~ parsed_dates.isna()).sum() / len(ser)
+        parsed_dates = pd.to_datetime(ser, errors="coerce", format="%m/%d/%Y")
+        return (~parsed_dates.isna()).sum() / len(ser)
+
 
     def euro_dates_frac(ser):
-        parsed_dates = pd.to_datetime(ser, errors='coerce', format="%d/%m/%Y")
-        return (~ parsed_dates.isna()).sum() / len(ser)
+        parsed_dates = pd.to_datetime(ser, errors="coerce", format="%d/%m/%Y")
+        return (~parsed_dates.isna()).sum() / len(ser)
+
 
     class HeuristicFracs(ColAnalysis):
         provides_defaults = dict(
-            str_bool_frac=0, regular_int_parse_frac=0, strip_int_parse_frac=0, us_dates_frac=0)
+            str_bool_frac=0,
+            regular_int_parse_frac=0,
+            strip_int_parse_frac=0,
+            us_dates_frac=0,
+        )
 
         @staticmethod
         def series_summary(sampled_ser, ser):
-            if not (pd.api.types.is_string_dtype(ser) or pd.api.types.is_object_dtype(ser)):
+            if not (
+                pd.api.types.is_string_dtype(ser)
+                or pd.api.types.is_object_dtype(ser)
+            ):
                 return {}
 
             return dict(
                 str_bool_frac=str_bool_frac(ser),
                 regular_int_parse_frac=int_parse_frac(ser),
                 strip_int_parse_frac=strip_int_parse_frac(ser),
-                us_dates_frac=us_dates_frac(ser))
+                us_dates_frac=us_dates_frac(ser),
+            )
     return (
         BOOL_SYNONYMS,
         ColAnalysis,
@@ -173,6 +274,8 @@ def _(np, pd):
 def _(ColAnalysis):
     from buckaroo.jlisp.lisp_utils import s, sA
     from buckaroo.auto_clean.heuristic_lang import get_top_score
+
+
     class BaseHeuristicCleaningGenOps(ColAnalysis):
         """
         This class is meant to be extended with different rules passed in
@@ -181,8 +284,9 @@ def _(ColAnalysis):
 
         Then put this group of classes into their own AutocleaningConfig
         """
+
         requires_summary = []
-        provides_defaults = {'cleaning_ops': []}
+        provides_defaults = {"cleaning_ops": []}
 
         rules = {}
         rules_op_names = {}
@@ -190,43 +294,70 @@ def _(ColAnalysis):
         @classmethod
         def computed_summary(kls, column_metadata):
             cleaning_op_name = get_top_score(kls.rules, column_metadata)
-            if cleaning_op_name == 'none':
-                return {'cleaning_ops': [], 'cleaning_name':'None'}
+            if cleaning_op_name == "none":
+                return {"cleaning_ops": [], "cleaning_name": "None"}
             else:
-                cleaning_name = kls.rules_op_names.get(cleaning_op_name, cleaning_op_name)
-                ops = [sA(cleaning_name, clean_strategy= kls.__name__, clean_col=column_metadata['col_name'] ),
-                    {'symbol': 'df'}]
-                return {'cleaning_ops':ops, 'cleaning_name':cleaning_name, 'add_orig': True}
+                cleaning_name = kls.rules_op_names.get(
+                    cleaning_op_name, cleaning_op_name
+                )
+                ops = [
+                    sA(
+                        cleaning_name,
+                        clean_strategy=kls.__name__,
+                        clean_col=column_metadata["col_name"],
+                    ),
+                    {"symbol": "df"},
+                ]
+                return {
+                    "cleaning_ops": ops,
+                    "cleaning_name": cleaning_name,
+                    "add_orig": True,
+                }
     return BaseHeuristicCleaningGenOps, get_top_score, s, sA
 
 
 @app.cell
 def _(BaseHeuristicCleaningGenOps, s):
-    frac_name_to_command =  {
-            'str_bool_frac':            'str_bool',
-            'regular_int_parse_frac':   'regular_int_parse',
-            'strip_int_parse_frac':     'strip_int_parse',
-            'us_dates_frac':            'us_date'}
+    frac_name_to_command = {
+        "str_bool_frac": "str_bool",
+        "regular_int_parse_frac": "regular_int_parse",
+        "strip_int_parse_frac": "strip_int_parse",
+        "us_dates_frac": "us_date",
+    }
+
 
     class ConvservativeCleaningGenops(BaseHeuristicCleaningGenOps):
-        requires_summary = ['str_bool_frac', 'regular_int_parse_frac', 'strip_int_parse_frac', 'us_dates_frac']
+        requires_summary = [
+            "str_bool_frac",
+            "regular_int_parse_frac",
+            "strip_int_parse_frac",
+            "us_dates_frac",
+        ]
 
         rules = {
-            'str_bool_frac':          [s('m>'), .9],
-            'regular_int_parse_frac': [s('m>'), .9],
-            'strip_int_parse_frac':   [s('m>'), .9],
-            'none':                   [s('none-rule')],
-            'us_dates_frac':          [s('primary'), [s('m>'), .8]]}
+            "str_bool_frac": [s("m>"), 0.9],
+            "regular_int_parse_frac": [s("m>"), 0.9],
+            "strip_int_parse_frac": [s("m>"), 0.9],
+            "none": [s("none-rule")],
+            "us_dates_frac": [s("primary"), [s("m>"), 0.8]],
+        }
         rules_op_names = frac_name_to_command
 
+
     class AggresiveCleaningGenOps(BaseHeuristicCleaningGenOps):
-        requires_summary = ['str_bool_frac', 'regular_int_parse_frac', 'strip_int_parse_frac', 'us_dates_frac']
+        requires_summary = [
+            "str_bool_frac",
+            "regular_int_parse_frac",
+            "strip_int_parse_frac",
+            "us_dates_frac",
+        ]
         rules = {
-            'str_bool_frac':          [s('m>'), .6],
-            'regular_int_parse_frac': [s('m>'), .9],
-            'strip_int_parse_frac':   [s('m>'), .75],
-            'none':                   [s('none-rule')],
-            'us_dates_frac':          [s('primary'), [s('m>'), .7]]}
+            "str_bool_frac": [s("m>"), 0.6],
+            "regular_int_parse_frac": [s("m>"), 0.9],
+            "strip_int_parse_frac": [s("m>"), 0.75],
+            "none": [s("none-rule")],
+            "us_dates_frac": [s("primary"), [s("m>"), 0.7]],
+        }
 
         rules_op_names = frac_name_to_command
     return (
@@ -262,31 +393,48 @@ def _(
     class AggressiveAC(AutocleaningConfig):
         autocleaning_analysis_klasses = [HeuristicFracs, AggresiveCleaningGenOps]
         command_klasses = [
-            IntParse, StripIntParse, StrBool, USDate,
-            DropCol, FillNA, GroupBy, NoOp,
-            Search]
+            IntParse,
+            StripIntParse,
+            StrBool,
+            USDate,
+            DropCol,
+            FillNA,
+            GroupBy,
+            NoOp,
+            Search,
+        ]
 
         quick_command_klasses = [Search]
-        name="aggressive"
+        name = "aggressive"
+
 
     class ConservativeAC(AggressiveAC):
-        autocleaning_analysis_klasses = [HeuristicFracs, ConvservativeCleaningGenops]
-        name="conservative"
+        autocleaning_analysis_klasses = [
+            HeuristicFracs,
+            ConvservativeCleaningGenops,
+        ]
+        name = "conservative"
+
 
     class CleaningDetailStyling(DefaultMainStyling):
         df_display_name = "cleaning_detail"
-        pinned_rows=[
-            obj_('dtype'), pinned_histogram(),
-            float_('str_bool_frac'), float_('regular_int_parse_frac'),
-            float_('strip_int_parse_frac'),float_('us_dates_frac'),
-            obj_('cleaning_name'),
-            obj_('null_count')
+        pinned_rows = [
+            obj_("dtype"),
+            pinned_histogram(),
+            float_("str_bool_frac"),
+            float_("regular_int_parse_frac"),
+            float_("strip_int_parse_frac"),
+            float_("us_dates_frac"),
+            obj_("cleaning_name"),
+            obj_("null_count"),
         ]
 
 
     class ACBuckaroo(BuckarooInfiniteWidget):
         autoclean_conf = tuple([NoCleaningConf, AggressiveAC, ConservativeAC])
-        analysis_klasses = copy_extend(BuckarooInfiniteWidget.analysis_klasses, CleaningDetailStyling)
+        analysis_klasses = copy_extend(
+            BuckarooInfiniteWidget.analysis_klasses, CleaningDetailStyling
+        )
     return ACBuckaroo, AggressiveAC, CleaningDetailStyling, ConservativeAC
 
 
