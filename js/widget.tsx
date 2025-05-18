75873333d8965636b4021c57a30abfb164fb0e57
Paddy Mullen <paddy@paddymullen.com>
2025-05-18 11:52:39 -0400

    Feat/active col render perf (#383)
    * released 0.10.4
    * Much better rendering performance
    * No longer run react in Strict Mode, better performance
    * Much smarter way of choosing highlighting the active column which took the re-render time from 360ms to 60ms.

--- js/widget.tsx
+++ js/widget.tsx
@@ -1,9 +1,111 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import { useState } from "react";
-import { createRender, useModelState, useModel } from "@anywidget/react";
 import srt from "buckaroo-js-core";
 import "./widget.css";
 import "../packages/buckaroo-js-core/dist/style.css";
+
+
+
+/**
+ * @template {Record<string, any>} T
+ * @typedef RenderContext
+ * @property {import("@anywidget/types").AnyModel<T>} model
+ * @property {import("@anywidget/types").Experimental} experimental
+ */
+
+/** @type {React.Context<RenderContext<any>>} */
+let RenderContext = React.createContext(/** @type {any} */ (null));
+
+/**
+ * @returns {RenderContext<any>}
+ */
+function useRenderContext() {
+	let ctx = React.useContext(RenderContext);
+	if (!ctx) throw new Error("RenderContext not found");
+	return ctx;
+}
+
+/**
+ * @template {Record<string, any>} T
+ * @returns {import("@anywidget/types").AnyModel<T>}
+ */
+export function useModel() {
+	let ctx = useRenderContext();
+	return ctx.model;
+}
+
+/** @returns {import("@anywidget/types").Experimental} */
+export function useExperimental() {
+	let ctx = useRenderContext();
+	return ctx.experimental;
+}
+
+/**
+ * A React Hook to use model-backed state in a component.
+ *
+ * Mirrors the API of `React.useState`, but synchronizes state with
+ * the underlying model provded by an anywidget host.
+ *
+ * @example
+ * ```ts
+ * import * as React from "react";
+ * import { useModelState } from "@anywidget/react";
+ *
+ * function Counter() {
+ *   let [value, setValue] = useModelState<number>("value");
+ *
+ *   return (
+ *     <button onClick={() => setValue((v) => v + 1)}>
+ *       Count: {value}
+ *     </button>
+ *   );
+ * }
+ * ```
+ *
+ * @template S
+ * @param {string} key - The name of the model field to use
+ * @returns {[S, React.Dispatch<React.SetStateAction<S>>]}
+ */
+export function useModelState(key) {
+	let model = useModel();
+	let value = React.useSyncExternalStore(
+		(update) => {
+			model.on(`change:${key}`, update);
+			return () => model.off(`change:${key}`, update);
+		},
+		() => model.get(key),
+	);
+	/** @type {React.Dispatch<React.SetStateAction<S>>} */
+	let setValue = React.useCallback(
+		(value) => {
+			model.set(
+				key,
+				// @ts-expect-error - TS cannot correctly narrow type
+				typeof value === "function" ? value(model.get(key)) : value,
+			);
+			model.save_changes();
+		},
+		[model, key],
+	);
+	return [value, setValue];
+}
+
+//patched version of createRender that doesn't use strict mode
+// increased performance and danger
+function createRender(Widget) {
+	return ({ el, model, experimental }) => {
+		let root = ReactDOM.createRoot(el);
+		root.render(
+				React.createElement(
+					RenderContext.Provider,
+					{ value: { model, experimental } },
+					React.createElement(Widget),
+				),
+		);
+		return () => root.unmount();
+	};
+}
 
 const renderDFV = createRender(() => {
 	console.log("renderDFV");
