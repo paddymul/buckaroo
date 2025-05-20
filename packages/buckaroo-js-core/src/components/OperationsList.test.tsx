eabaf4ae88a91a10b31385e2d39294cf735d33cd
Paddy Mullen <paddy@paddymullen.com>
2025-05-20 08:31:49 -0400

    vite urrently runs cleanly

--- packages/buckaroo-js-core/src/components/OperationsList.test.tsx
+++ packages/buckaroo-js-core/src/components/OperationsList.test.tsx
@@ -1,4 +1,5 @@
 //import React from 'react';
+import '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { OperationsList2 } from './OperationsList';
--- packages/buckaroo-js-core/vite.config.ts
+++ packages/buckaroo-js-core/vite.config.ts
@@ -12,12 +12,12 @@ export default defineConfig({
             name: "vite-react-ts-button", // Sets the name of the generated library.
             fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
             formats: ["cjs", "es", "esm"], // Specifies the output formats (CommonJS and ES modules).
+	    exclude: ['**/*.test.ts','**/*.test.tsx'],
         },
         rollupOptions: {
-            //external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
             external: ["react", "react-dom", 'react/jsx-runtime'],
-            globals : { react : 'React', 'react-dom': 'react-dom'},
         },
+	exclude: ['./src/**/*.test.ts', './src/**/*.test.tsx', 'src/components/OperationsList.test.tsx'],
         sourcemap: true, // Generates source maps for debugging.
         emptyOutDir: true, // Clears the output directory before building.
         minify: false
