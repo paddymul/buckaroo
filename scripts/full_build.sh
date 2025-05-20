cac9b740ade186639931632aa60a9679f4fd149e
Paddy Mullen <paddy@paddymullen.com>
2025-05-20 08:18:19 -0400

    full build works

--- scripts/full_build.sh
+++ scripts/full_build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
- set -e
-rm -rf node_modules buckaroo/widget.js
-rm -rf  packages/buckaroo-js-core/dist packages/buckaroo-js-core/node_modules
+set -e
+rm -rf node_modules buckaroo/widget.js || true
+rm -rf  packages/buckaroo-js-core/dist packages/buckaroo-js-core/node_modules || true
 cd packages/buckaroo-js-core
-npm install && npm run build
+pnpm install
+pnpm run build:tsc
+pnpm run build:vite 
 cd ../..
 cp packages/buckaroo-js-core/dist/style.css buckaroo/static/compiled.css
-rm -rf packages/buckaroo-js-core/node_modules
-npm install && npm run build
-rm -rf dist
+rm -rf packages/buckaroo-js-core/node_modules || true
+pnpm install && pnpm run build
+rm -rf dist || true
 uv build --wheel
 #time hatch build
 #python -m twine upload --repository pypi dist/*.whl
