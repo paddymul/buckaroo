147e52cc0245dddf2b6046393368d791f8431e6f
Paddy Mullen <paddy@paddymullen.com>
2025-06-07 16:28:10 -0400

    WIP

---
+++ buckaroo/ddd_library.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+"""
+THe dastardly dataframe dataset.
+
+The weirdest dataframes that cause trouble frquently
+
+"""
+
+def get_basic_df():
+    return pd.DataFrame({'a':[10,20,30]})
+
+def get_multi_index_cols(rows=15) -> pd.DataFrame:
+    cols = pd.MultiIndex.from_tuples(
+    [('foo', 'a'), ('foo', 'b'),  ('bar', 'a'), ('bar', 'b'), ('bar', 'c')])
+    return pd.DataFrame(
+        [["asdf","foo_b", "bar_a", "bar_b", "bar_c"]] * rows,
+        columns=cols)
+def get_tuple_cols(rows=15) -> pd.DataFrame:
+    multi_col_df = get_multi_index_cols(rows)
+    multi_col_df.columns = multi_col_df.columns.to_flat_index()
+    return multi_col_df
