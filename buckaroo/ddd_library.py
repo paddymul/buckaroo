357bb3afa928b4009f5c23aeb647c22804551589
Paddy Mullen <paddy@paddymullen.com>
2025-06-08 10:43:25 -0400

    WIP, failing targetted multi_index_test

--- buckaroo/ddd_library.py
+++ buckaroo/ddd_library.py
@@ -10,13 +10,13 @@ The weirdest dataframes that cause trouble frquently
 def get_basic_df():
     return pd.DataFrame({'a':[10,20,30]})
 
-def get_multi_index_cols(rows=15) -> pd.DataFrame:
+def get_multi_index_cols_df(rows=15) -> pd.DataFrame:
     cols = pd.MultiIndex.from_tuples(
     [('foo', 'a'), ('foo', 'b'),  ('bar', 'a'), ('bar', 'b'), ('bar', 'c')])
     return pd.DataFrame(
         [["asdf","foo_b", "bar_a", "bar_b", "bar_c"]] * rows,
         columns=cols)
-def get_tuple_cols(rows=15) -> pd.DataFrame:
+def get_tuple_cols_df(rows=15) -> pd.DataFrame:
     multi_col_df = get_multi_index_cols(rows)
     multi_col_df.columns = multi_col_df.columns.to_flat_index()
     return multi_col_df
--- tests/unit/dataflow/styling_core_tests.py
+++ tests/unit/dataflow/styling_core_tests.py
@@ -1,7 +1,8 @@
 from typing import List
-import pytest
 import pandas as pd
 from buckaroo.dataflow.styling_core import ColumnConfig, DFViewerConfig, StylingAnalysis
+from buckaroo.ddd_library import get_multi_index_cols_df
+from buckaroo.pluggable_analysis_framework.pluggable_analysis_framework import SDType
 
 
 def test_simple_styling() -> None:
@@ -15,3 +16,20 @@ def test_simple_styling() -> None:
     assert len(col_config) == 3
     assert col_config[1]['col_name'] == 'a'
     assert col_config[2]['col_name'] == 'b'
+
+def test_multi_index_styling() -> None:
+
+    mic_df: pd.DataFrame = get_multi_index_cols_df()
+    fake_sd:SDType = {
+        "('foo', 'a')": {},
+        "('foo', 'b')": {},
+        "('bar', 'a')": {}
+    }
+
+    dfvc: DFViewerConfig = StylingAnalysis.style_columns(fake_sd, mic_df)
+
+    col_config: List[ColumnConfig] = dfvc['column_config']
+    assert len(col_config) == 4
+    assert col_config[1]['col_path'] == ('foo', 'a')
+    assert col_config[2]['col_path'] == ('foo', 'b')
+    assert col_config[3]['col_path'] == ('bar', 'a')
