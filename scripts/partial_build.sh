3ee787428fc3b70ec1a0523c18617dd7ab3217fe
Paddy Mullen <paddy@paddymullen.com>
2025-06-15 11:59:31 -0400

    working fast parital_build script

--- scripts/partial_build.sh
+++ scripts/partial_build.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 #partial doesn't delete node_modules
+
+
+mv node_modules temp_node_modules
 cd packages/buckaroo-js-core
-pnpm install && pnpm run build
+pnpm install
+pnpm run build:tsc
+pnpm run build:vite 
 cd ../..
 cp packages/buckaroo-js-core/dist/style.css buckaroo/static/compiled.css
+mv packages/buckaroo-js-core/node_modules  packages/buckaroo-js-core/temp_node_modules
 pnpm install && pnpm run build
 rm -rf dist
 uv build --wheel
+mv temp_node_modules node_modules 
+mv packages/buckaroo-js-core/temp_node_modules packages/buckaroo-js-core/node_modules  
 #time hatch build
 #python -m twine upload --repository pypi dist/*.whl
 
