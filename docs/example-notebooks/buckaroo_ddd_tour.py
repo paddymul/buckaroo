e432a39e380bb338584bca378be86ed4bf898d03
Paddy Mullen <paddy@paddymullen.com>
2025-07-18 16:17:53 -0400

    removed other cruft

--- docs/example-notebooks/buckaroo_ddd_tour.py
+++ docs/example-notebooks/buckaroo_ddd_tour.py
@@ -43,70 +43,8 @@ def _(buckaroo, mo):
 
 
 @app.cell
-def _(ddd, disp):
-    disp(ddd.get_basic_df())
-    return
-
-
-@app.cell
-def _(ddd, disp):
-    disp(ddd.get_multiindex_index_df())
-    return
-
-
-@app.cell
 def _():
     #mo.ui.code_editor(buckaroo.BuckarooInfiniteWidget(ddd.get_multiindex_index_df()).get_story_config())
-    return
-
-
-@app.cell
-def _(ddd, disp):
-    disp(ddd.get_multiindex_index_df())
-    return
-
-
-@app.cell
-def _(ddd, disp):
-    disp(ddd.get_multiindex_with_names_index_df())
-    return
-
-
-@app.cell
-def _(ddd, disp):
-    disp(ddd.get_multiindex_index_multiindex_with_names_cols_df())
-    return
-
-
-@app.cell
-def _(ddd, disp):
-    disp(ddd.get_multiindex_index_with_names_multiindex_cols_df())
-    # for single level indexes, use header_name, not one level col_path
-    # index columns should be pinned also
-    return
-
-
-@app.cell
-def _(ddd, disp):
-    disp(ddd.get_multiindex_with_names_both())
-    return
-
-
-@app.cell
-def _(ddd, mo):
-    mo.plain(ddd.get_multiindex_with_names_both())
-    return
-
-
-@app.cell
-def _(ddd, disp):
-    disp(ddd.get_multiindex3_index_df())
-    return
-
-
-@app.cell
-def _(ddd, disp):
-    disp(ddd.df_with_infinity())
     return
 
 
