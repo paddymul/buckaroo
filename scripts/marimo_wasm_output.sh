f53d7168b0a75e76d19cb2afb58150938f186fc3
Paddy Mullen <paddy@paddymullen.com>
2025-08-04 15:12:07 -0400

    three output notebooks

--- scripts/marimo_wasm_output.sh
+++ scripts/marimo_wasm_output.sh
@@ -5,6 +5,5 @@ notebook_name="${filename%.*}"
 
 full_notebook_path="docs/example-notebooks/marimo-wasm/${filename}"
 full_output_path="docs/extra-html/example_notebooks/${notebook_name}"
-echo "mode $mode"
 uv run marimo export html-wasm $full_notebook_path   -o $full_output_path   --mode $mode -f
 #uv run marimo export html-wasm docs/example-notebooks/buckaroo_ddd_tour.py  -o docs/extra-html/buckaroo_ddd_tour  --mode run
