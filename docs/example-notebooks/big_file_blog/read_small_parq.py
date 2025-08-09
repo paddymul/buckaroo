489ebef4d3445363e9149b093e904fa7295d23d1
Paddy Mullen <paddy@paddymullen.com>
2025-08-09 11:02:57 -0400

    more consolidation

--- docs/example-notebooks/big_file_blog/read_small_parq.py
+++ docs/example-notebooks/big_file_blog/read_small_parq.py
@@ -7,3 +7,12 @@ def to_parquet(orig_csv_fname, output_parquet_fname):
 
 to_parquet("~/NPPES_Data_Dissemination_July_2025/npidata_pfile_20050523-20250713.csv", "~/JULY_FULL2.parq")
 time.sleep(30)
+
+'''
+#contents of write_parq.py
+import polars as pl
+JULY_FILE = "~/NPPES_Data_Dissemination_July_2025/npidata_pfile_20050523-20250713.csv"
+pl.read_csv(JULY_FILE).write_parquet("~/JULY_FULL2.parq")
+
+
+'''
--- docs/example-notebooks/big_file_blog/write_parq.py
+++
@@ -1,3 +0,0 @@
-import polars as pl
-JULY_FILE = "~/NPPES_Data_Dissemination_July_2025/npidata_pfile_20050523-20250713.csv"
-pl.read_csv(JULY_FILE).write_parquet("~/JULY_FULL2.parq")
