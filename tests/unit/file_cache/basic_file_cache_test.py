dcccb70611884bdf2c33ec39ae40be70899931f4
Paddy Mullen <paddy@paddymullen.com>
2025-10-16 16:03:19 -0400

    specific tests to make sure that the right dat gets into the cache

--- tests/unit/file_cache/basic_file_cache_test.py
+++ tests/unit/file_cache/basic_file_cache_test.py
@@ -1,6 +1,7 @@
 #from buckaroo.file_cache import base
 from buckaroo.file_cache.base import ColGroup, ProgressNotification, ColumnResults, ColumnResult, FileCache, Executor
 import polars as pl
+import polars.selectors as cs
 from typing import cast
 # fc = FileCache()    
 
@@ -30,28 +31,34 @@ def simple_column_func(ldf:pl.LazyFrame, cols:ColGroup) -> ColumnResults:
     only_cols_ldf = ldf.select(cols)
     res = only_cols_ldf.select(
     pl.all().pl_series_hash.hash_xx().name.suffix("_hash"),
+    #pl.col(pl.NUMERIC_DTYPES)
+    cs.numeric().sum().name.suffix("_sum"),
     pl.all().len().name.suffix("_len")).collect()
     
     col_results:ColumnResults = {}
     for col in cols:
 
         hash_:int = cast(int, res[col+"_hash"][0])
+        if col+"_sum" in res.columns:
+            actual_result = {'len':res[col+"_len"][0], 'sum':res[col+"_sum"][0] }
+        else:
+            actual_result = {'len':res[col+"_len"][0]}
+            
         cr = ColumnResult(
             series_hash=hash_,
             column_name=col,
             expressions=[],
-            result={'len':res[col+"_len"][0]})
+            result=actual_result)
         col_results[col] = cr
     return col_results
 
+df = pl.DataFrame({
+    'a1': [10,20,30],
+    'b2': ["foo", "bar", "baz"]
+    })
+ldf = df.lazy()
         
 def test_simple_executor():
-
-    df = pl.DataFrame({
-        'a1': [10,20,30],
-        'b2': ["foo", "bar", "baz"]
-        })
-    ldf = df.lazy()
 
     fc = FileCache()
     call_count = [0]
@@ -62,6 +69,25 @@ def test_simple_executor():
 
     exc = Executor(ldf, simple_column_func, [], listener, fc)
     exc.run()
-
     assert call_count[0] == 2
-    
+    assert fc.get_series_results(13038993034761730339) == {'len':3, 'sum':60}
+
+    assert fc.get_series_results(1505513022777147474) == {'len':3}
+    # print(fc.summary_stats_cache)
+    # 1/0
+#     Here58 True {'a1': ColumnResult(series_hash=13038993034761730339, column_name='a1', expressions=[], result={'len': 3})}
+# here58 True {'b2': ColumnResult(series_hash=1505513022777147474, column_name='b2', expressions=[], result={'len': 3})}
+
+
+def test_simple_executor_listener_calls():
+    fc = FileCache()
+    call_count = [0]
+    def listener(progress:ProgressNotification) -> None:
+        print("here58", progress.success, progress.result)
+        call_count[0]+=1
+
+
+    exc = Executor(ldf, simple_column_func, [], listener, fc)
+    exc.run()
+    assert call_count[0] == 2
+
