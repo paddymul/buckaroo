3d219ab8cd3647a640473a28e0f7c38aece81f46
Paddy Mullen <paddy@paddymullen.com>
2025-10-23 17:06:25 -0400

    more comments on timing

--- buckaroo/file_cache/simple_multiprocessing_timeout.py
+++ buckaroo/file_cache/simple_multiprocessing_timeout.py
@@ -71,31 +71,6 @@ def func_runner(*args2, func_name=None, que=None, **kwargs3):
     que.put((func_name, res,))
     print("func_runner finish")
 
-def mp_timeout(orig_f, timeout_secs=3):
-
-    func_name = orig_f.__name__
-    funcs[func_name] = orig_f
-
-
-    def actual_func(*args, **kwargs):
-        print("actual_func start")
-        kwargs2 = kwargs.copy()
-        kwargs2['que'] = q
-        kwargs2['func_name'] = func_name
-        p = mp.Process(target=func_runner, args=args, kwargs=kwargs2)
-        p.start()
-        p.join(timeout_secs)
-        try:
-            name, outer_res = q.get_nowait()
-            print("name")
-            return outer_res
-        except Exception as e:
-            print(e)
-            p.terminate()
-            p.join()
-            raise
-    return actual_func
-
 def mp_timeout_dec(timeout_secs):
     def mp_timeout(orig_f):
 
@@ -132,26 +107,38 @@ def sleep_2_return():
     print("sleep_2_return after sleep")
     return 2
 
-dec_sleep2 = mp_timeout(sleep_2_return, 3)
-dec_sleep4 = mp_timeout(sleep_4_return, 3)
-
-dec_sleep4 = mp_timeout(sleep_4_return, 3)
+# dec_sleep2 = mp_timeout(sleep_2_return, 3)
+# dec_sleep4 = mp_timeout(sleep_4_return, 3)
+# dec_sleep4 = mp_timeout(sleep_4_return, 3)
 
 @mp_timeout_dec(1)        
 def sleep_4_return2():
     time.sleep(4)
     return 4.5
 
-        
+
+@mp_timeout_dec(1)
+def return_1():
+    #time.sleep(.5)
+    return 1
+
+def raw_return_1():
+    return 1
+
+
 if __name__ == "__main__":
-    print(sleep_4_return2())
-    
-    # print("here")
-    # sleep2_res = dec_sleep2()
-    # print("sleep2_res", sleep2_res)
+    #print(sleep_4_return2())
+    #print(return_1())
+    pass
+    # for i in range(100):
+    #     print(i, return_1())
+        #decorator timing
+        #python simple_multiprocessing_timeout.py  2.43s user 0.66s system 97% cpu 3.160 total
 
-    # sleep4_res = dec_sleep4()
-    # print("sleep3_res", sleep4_res)
-    
 
-    #main(3.0)
+
+        # print(raw_return_1())
+        # raw return timing
+        #python simple_multiprocessing_timeout.py  0.04s user 0.02s system 70% cpu 0.079 total
+
+        #so each fork takes approximately 30ms. which is signficant, but workable
