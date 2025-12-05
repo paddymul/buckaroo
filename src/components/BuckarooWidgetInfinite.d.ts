aad16446f598db179efa97663b33db6ab2c932bf
paddymul <paddymul@users.noreply.github.com>
2025-12-05 19:38:18 +0000

    deploy: db30e215fad3780214c69e399b9c55c0d82838cc

--- src/components/BuckarooWidgetInfinite.d.ts
+++ src/components/BuckarooWidgetInfinite.d.ts
@@ -1,4 +1,3 @@
-import { default as React } from '../../node_modules/.pnpm/react@18.3.1/node_modules/react';
 import { OperationResult } from './DependentTabs';
 import { DFData } from './DFViewerParts/DFWhole';
 import { BuckarooState, BuckarooOptions, DFMeta } from './WidgetTypes';
@@ -23,16 +22,10 @@ export declare function BuckarooInfiniteWidget({ df_data_dict, df_display_args, 
     buckaroo_options: BuckarooOptions;
     src: KeyAwareSmartRowCache;
 }): import("react/jsx-runtime").JSX.Element;
-export declare function DFViewerInfiniteDS({ df_meta, df_data_dict, df_display_args, src, df_id, message_log, show_message_box }: {
+export declare function DFViewerInfiniteDS({ df_meta, df_data_dict, df_display_args, src, df_id }: {
     df_meta: DFMeta;
     df_data_dict: Record<string, DFData>;
     df_display_args: Record<string, IDisplayArgs>;
     src: KeyAwareSmartRowCache;
     df_id: string;
-    message_log?: {
-        messages?: Array<any>;
-    };
-    show_message_box?: {
-        enabled?: boolean;
-    };
 }): import("react/jsx-runtime").JSX.Element;
--- src/components/MessageBox.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function MessageBox({ messages, }: {
-    messages: Array<{
-        time?: string;
-        type?: string;
-        message?: string;
-        [key: string]: any;
-    }>;
-}): import("react/jsx-runtime").JSX.Element | null;
